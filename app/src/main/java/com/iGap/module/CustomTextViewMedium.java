354f977c3c536f76cdc1b2b261836e3308bf1ffa
Alireza <eskandarpour@rooyekhat.co>
2016-09-04 11:14:18 +0430

    improved with better font caching

--- app/src/main/java/com/iGap/module/CustomTextViewMedium.java
+++ app/src/main/java/com/iGap/module/CustomTextViewMedium.java
@@ -1,27 +1,28 @@
 package com.iGap.module;
 
 import android.content.Context;
-import android.graphics.Typeface;
 import android.util.AttributeSet;
 import android.widget.TextView;
 
+import com.iGap.helper.FontCache;
+
 public class CustomTextViewMedium extends TextView {
-    public static Typeface FONT_NAME;
-
-
     public CustomTextViewMedium(Context context) {
         super(context);
-        if(FONT_NAME == null) FONT_NAME = Typeface.createFromAsset(context.getAssets(), "fonts/IRANSansMedium.ttf");
-        this.setTypeface(FONT_NAME);
+        init(context);
     }
+
     public CustomTextViewMedium(Context context, AttributeSet attrs) {
         super(context, attrs);
-        if(FONT_NAME == null) FONT_NAME = Typeface.createFromAsset(context.getAssets(), "fonts/IRANSansMedium.ttf");
-        this.setTypeface(FONT_NAME);
+        init(context);
     }
+
     public CustomTextViewMedium(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
-        if(FONT_NAME == null) FONT_NAME = Typeface.createFromAsset(context.getAssets(), "fonts/IRANSansMedium.ttf");
-        this.setTypeface(FONT_NAME);
+        init(context);
+    }
+
+    private void init(Context context) {
+        setTypeface(FontCache.get("fonts/IRANSansMedium.ttf", context));
     }
 }
