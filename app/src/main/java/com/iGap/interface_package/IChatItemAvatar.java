d52f1cb55368eb5129980fa7a157b54efcb2a186
Alireza Eskandarpour Shoferi <eskandarpour@rooyekhat.co>
2016-10-22 16:35:01 +0330

    IMPORTANT: moved protos into new module for better project structure and some improvements (proto buffer updated to v3.1.0)

---
+++ app/src/main/java/com/iGap/interface_package/IChatItemAvatar.java
@@ -0,0 +1,8 @@
+package com.iGap.interface_package;
+
+/**
+ * Created by Alireza Eskandarpour Shoferi (meNESS) on 9/28/2016.
+ */
+public interface IChatItemAvatar {
+    void onRequestDownloadAvatar();
+}
