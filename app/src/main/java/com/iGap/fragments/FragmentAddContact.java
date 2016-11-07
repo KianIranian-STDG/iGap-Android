931a82e8d85b4d2afa305368ab1a255cd998dbb8
Amir Molareza <molareza@rooyekhat.co>
2016-11-07 14:45:19 +0330

    add some change on add contact

--- app/src/main/java/com/iGap/fragments/FragmentAddContact.java
+++ app/src/main/java/com/iGap/fragments/FragmentAddContact.java
@@ -4,6 +4,7 @@ import android.content.ContentProviderOperation;
 import android.content.Context;
 import android.os.Bundle;
 import android.provider.ContactsContract;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.text.Editable;
 import android.text.TextWatcher;
@@ -15,6 +16,7 @@ import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.afollestad.materialdialogs.DialogAction;
 import com.afollestad.materialdialogs.MaterialDialog;
 import com.iGap.G;
 import com.iGap.R;
@@ -32,6 +34,7 @@ public class FragmentAddContact extends android.support.v4.app.Fragment {
     private EditText edtPhoneNumber;
     private ViewGroup parent;
     private RippleView rippleSet;
+    private MaterialDesignTextView txtSet;
 
     public static FragmentAddContact newInstance() {
         return new FragmentAddContact();
@@ -52,8 +55,7 @@ public class FragmentAddContact extends android.support.v4.app.Fragment {
 
     private void initComponent(final View view) {
 
-        MaterialDesignTextView btnBack =
-                (MaterialDesignTextView) view.findViewById(R.id.ac_txt_back);
+        MaterialDesignTextView btnBack = (MaterialDesignTextView) view.findViewById(R.id.ac_txt_back);
         RippleView rippleBack = (RippleView) view.findViewById(R.id.ac_ripple_back);
         rippleBack.setOnRippleCompleteListener(new RippleView.OnRippleCompleteListener() {
             @Override
@@ -71,7 +73,8 @@ public class FragmentAddContact extends android.support.v4.app.Fragment {
         });
 
 
-        MaterialDesignTextView txtSet = (MaterialDesignTextView) view.findViewById(R.id.ac_txt_set);
+        txtSet = (MaterialDesignTextView) view.findViewById(R.id.ac_txt_set);
+        txtSet.setTextColor(getResources().getColor(R.color.gray_9d));
 
         parent = (ViewGroup) view.findViewById(R.id.ac_layoutParent);
         parent.setOnClickListener(new View.OnClickListener() {
@@ -142,6 +145,38 @@ public class FragmentAddContact extends android.support.v4.app.Fragment {
                 isEnableSetButton();
             }
         });
+        edtLastName.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                isEnableSetButton();
+            }
+        });
+        edtPhoneNumber.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                isEnableSetButton();
+            }
+        });
 
         getActivity().getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN);
         rippleSet = (RippleView) view.findViewById(R.id.ac_ripple_set);
@@ -154,76 +189,81 @@ public class FragmentAddContact extends android.support.v4.app.Fragment {
                         .title(R.string.add_to_list_contact)
                         .content(R.string.text_add_to_list_contact)
                         .positiveText(R.string.B_ok)
+                        .onPositive(new MaterialDialog.SingleButtonCallback() {
+                            @Override
+                            public void onClick(@NonNull MaterialDialog dialog, @NonNull DialogAction which) {
+                                if (edtFirstName.getText().toString().length() > 0 || edtLastName.getText().toString().length() > 0) {
+                                    if (edtPhoneNumber.getText().toString().length() > 0) {
+
+                                        String displayName =
+                                                edtFirstName.getText().toString() + " " + edtLastName.getText()
+                                                        .toString();
+                                        String phone = edtPhoneNumber.getText().toString();
+
+                                        ArrayList<ContentProviderOperation> ops =
+                                                new ArrayList<ContentProviderOperation>();
+
+                                        ops.add(ContentProviderOperation.newInsert(
+                                                ContactsContract.RawContacts.CONTENT_URI)
+                                                .withValue(ContactsContract.RawContacts.ACCOUNT_TYPE, null)
+                                                .withValue(ContactsContract.RawContacts.ACCOUNT_NAME, null)
+                                                .build());
+
+                                        //------------------------------------------------------ Names
+                                        if (displayName != null) {
+                                            ops.add(ContentProviderOperation.newInsert(
+                                                    ContactsContract.Data.CONTENT_URI)
+                                                    .withValueBackReference(ContactsContract.Data.RAW_CONTACT_ID, 0)
+                                                    .withValue(ContactsContract.Data.MIMETYPE,
+                                                            ContactsContract.CommonDataKinds.StructuredName.CONTENT_ITEM_TYPE)
+                                                    .withValue(
+                                                            ContactsContract.CommonDataKinds.StructuredName.DISPLAY_NAME,
+                                                            displayName)
+                                                    .build());
+                                        }
+                                        //------------------------------------------------------ Mobile Number
+                                        if (phone != null) {
+                                            ops.add(ContentProviderOperation.
+                                                    newInsert(ContactsContract.Data.CONTENT_URI)
+                                                    .withValueBackReference(ContactsContract.Data.RAW_CONTACT_ID, 0)
+                                                    .withValue(ContactsContract.Data.MIMETYPE,
+                                                            ContactsContract.CommonDataKinds.Phone.CONTENT_ITEM_TYPE)
+                                                    .withValue(ContactsContract.CommonDataKinds.Phone.NUMBER, phone)
+                                                    .withValue(ContactsContract.CommonDataKinds.Phone.TYPE,
+                                                            ContactsContract.CommonDataKinds.Phone.TYPE_MOBILE)
+                                                    .build());
+                                        }
+
+                                        try {
+                                            G.context.getContentResolver()
+                                                    .applyBatch(ContactsContract.AUTHORITY, ops);
+                                            addContactToServer();
+                                            Toast.makeText(G.context, R.string.save_ok, Toast.LENGTH_SHORT).show();
+                                        } catch (Exception e) {
+                                            e.printStackTrace();
+                                            Toast.makeText(G.context,
+                                                    getString(R.string.exception) + e.getMessage(), Toast.LENGTH_SHORT)
+                                                    .show();
+                                        }
+
+                                        getActivity().getSupportFragmentManager()
+                                                .beginTransaction()
+                                                .remove(FragmentAddContact.this)
+                                                .commit();
+                                    } else {
+                                        Toast.makeText(G.context, R.string.please_enter_phone_number,
+                                                Toast.LENGTH_SHORT).show();
+                                    }
+                                } else {
+                                    Toast.makeText(G.context, R.string.please_enter_firstname_or_lastname,
+                                            Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        })
                         .negativeText(R.string.B_cancel)
                         .show();
 
-                if (edtFirstName.getText().toString().length() > 0
-                        || edtLastName.getText().toString().length() > 0) {
-                    if (edtPhoneNumber.getText().toString().length() > 0) {
 
-                        String displayName =
-                                edtFirstName.getText().toString() + " " + edtLastName.getText()
-                                        .toString();
-                        String phone = edtPhoneNumber.getText().toString();
-
-                        ArrayList<ContentProviderOperation> ops =
-                                new ArrayList<ContentProviderOperation>();
-
-                        ops.add(ContentProviderOperation.newInsert(
-                                ContactsContract.RawContacts.CONTENT_URI)
-                                .withValue(ContactsContract.RawContacts.ACCOUNT_TYPE, null)
-                                .withValue(ContactsContract.RawContacts.ACCOUNT_NAME, null)
-                                .build());
-
-                        //------------------------------------------------------ Names
-                        if (displayName != null) {
-                            ops.add(ContentProviderOperation.newInsert(
-                                    ContactsContract.Data.CONTENT_URI)
-                                    .withValueBackReference(ContactsContract.Data.RAW_CONTACT_ID, 0)
-                                    .withValue(ContactsContract.Data.MIMETYPE,
-                                            ContactsContract.CommonDataKinds.StructuredName.CONTENT_ITEM_TYPE)
-                                    .withValue(
-                                            ContactsContract.CommonDataKinds.StructuredName.DISPLAY_NAME,
-                                            displayName)
-                                    .build());
-                        }
-                        //------------------------------------------------------ Mobile Number
-                        if (phone != null) {
-                            ops.add(ContentProviderOperation.
-                                    newInsert(ContactsContract.Data.CONTENT_URI)
-                                    .withValueBackReference(ContactsContract.Data.RAW_CONTACT_ID, 0)
-                                    .withValue(ContactsContract.Data.MIMETYPE,
-                                            ContactsContract.CommonDataKinds.Phone.CONTENT_ITEM_TYPE)
-                                    .withValue(ContactsContract.CommonDataKinds.Phone.NUMBER, phone)
-                                    .withValue(ContactsContract.CommonDataKinds.Phone.TYPE,
-                                            ContactsContract.CommonDataKinds.Phone.TYPE_MOBILE)
-                                    .build());
-                        }
-
-                        try {
-                            G.context.getContentResolver()
-                                    .applyBatch(ContactsContract.AUTHORITY, ops);
-                            addContactToServer();
-                            Toast.makeText(G.context, R.string.save_ok, Toast.LENGTH_SHORT).show();
-                        } catch (Exception e) {
-                            e.printStackTrace();
-                            Toast.makeText(G.context,
-                                    getString(R.string.exception) + e.getMessage(), Toast.LENGTH_SHORT)
-                                    .show();
-                        }
-
-                        getActivity().getSupportFragmentManager()
-                                .beginTransaction()
-                                .remove(FragmentAddContact.this)
-                                .commit();
-                    } else {
-                        Toast.makeText(G.context, R.string.please_enter_phone_number,
-                                Toast.LENGTH_SHORT).show();
-                    }
-                } else {
-                    Toast.makeText(G.context, R.string.please_enter_firstname_or_lastname,
-                            Toast.LENGTH_SHORT).show();
-                }
             }
         });
     }
@@ -231,10 +271,15 @@ public class FragmentAddContact extends android.support.v4.app.Fragment {
     private void isEnableSetButton() {
 
 
-        if (!edtFirstName.getText().toString().equals("") || !edtLastName.getText().toString().equals("") && !edtPhoneNumber.getText().toString().equals("")) {
+        if ((edtFirstName.getText().toString().length() > 0 || edtLastName.getText().toString().length() > 0)
+                && edtPhoneNumber.getText().toString().length() > 0) {
 
+            txtSet.setTextColor(getResources().getColor(R.color.white));
             rippleSet.setEnabled(true);
 
+        } else {
+            txtSet.setTextColor(getResources().getColor(R.color.gray_9d));
+            rippleSet.setEnabled(false);
         }
     }
 
