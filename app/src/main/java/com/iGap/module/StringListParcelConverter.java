17aa56fe083682fb85d0d7a39167b8154383a5f0
Saeed Mozaffari <mozafari@rooyekhat.co>
2017-02-12 17:18:47 +0330

    Clear BoomMenu From Gradle

--- app/src/main/java/com/iGap/module/StringListParcelConverter.java
+++ app/src/main/java/com/iGap/module/StringListParcelConverter.java
@@ -1,15 +1,9 @@
 package com.iGap.module;
 
 import android.os.Parcel;
-
+import com.iGap.activities.ActivityChat;
 import com.iGap.realm.RealmString;
-import com.nightonke.boommenu.Bar;
-
 import org.parceler.Parcels;
-
-/**
- * Created by Alireza Eskandarpour Shoferi (meNESS) on 11/1/2016.
- */
 
 // Specific class for a RealmList<Bar> field
 public class StringListParcelConverter extends RealmListParcelConverter<RealmString> {
@@ -21,6 +15,6 @@ public class StringListParcelConverter extends RealmListParcelConverter<RealmStr
 
     @Override
     public RealmString itemFromParcel(Parcel parcel) {
-        return Parcels.unwrap(parcel.readParcelable(Bar.class.getClassLoader()));
+        return Parcels.unwrap(parcel.readParcelable(ActivityChat.class.getClassLoader()));
     }
 }
