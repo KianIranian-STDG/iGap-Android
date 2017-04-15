252548e2bebe1d567201fe3b3652cd4aebbae141
Amir Molareza <molareza@rooyekhat.co>
2017-04-15 11:05:35 +0430

    fix compress for resolution below 640 x 480

--- compress/src/main/java/com/lalongooo/videocompressor/video/MediaController.java
+++ compress/src/main/java/com/lalongooo/videocompressor/video/MediaController.java
@@ -12,6 +12,11 @@ import android.media.MediaMetadataRetriever;
 import android.os.Build;
 import android.util.Log;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.nio.ByteBuffer;
 
 @SuppressLint("NewApi") public class MediaController {
@@ -254,8 +259,14 @@ import java.nio.ByteBuffer;
         int resultHeight;
 
         if (videoWidth * videoHeight < 307200) {
-            resultWidth = videoWidth;
-            resultHeight = videoHeight;
+            //resultWidth = videoWidth;
+            //resultHeight = videoHeight;
+            try {
+                copyFile(new File(path), new File(savePath));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return true;
         } else {
             resultWidth = 640;
             resultHeight = 480;
@@ -688,4 +699,19 @@ import java.nio.ByteBuffer;
         //inputFile.delete();
         return true;
     }
+
+    public static void copyFile(File src, File dst) throws IOException {
+        InputStream in = new FileInputStream(src);
+        OutputStream out = new FileOutputStream(dst);
+
+        // Transfer bytes from in to out
+        byte[] buf = new byte[1024];
+        int len;
+        while ((len = in.read(buf)) > 0) {
+            out.write(buf, 0, len);
+        }
+        in.close();
+        out.close();
+    }
+
 }
