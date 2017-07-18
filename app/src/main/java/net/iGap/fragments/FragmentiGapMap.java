48e947628293757533d47d558c6dbf4d400d3b06
Amir Molareza <molareza@rooyekhat.co>
2017-07-18 12:32:04 +0430

    some change

--- app/src/main/java/net/iGap/fragments/FragmentiGapMap.java
+++ app/src/main/java/net/iGap/fragments/FragmentiGapMap.java
@@ -25,6 +25,8 @@ import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.GestureDetector;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
@@ -35,6 +37,7 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.ToggleButton;
+import com.afollestad.materialdialogs.MaterialDialog;
 import java.util.ArrayList;
 import java.util.List;
 import net.iGap.Config;
@@ -43,9 +46,11 @@ import net.iGap.R;
 import net.iGap.interfaces.OnGetNearbyCoordinate;
 import net.iGap.interfaces.OnLocationChanged;
 import net.iGap.libs.rippleeffect.RippleView;
+import net.iGap.module.DialogAnimation;
 import net.iGap.module.GPSTracker;
 import net.iGap.module.MyInfoWindow;
 import net.iGap.proto.ProtoGeoGetNearbyCoordinate;
+import net.iGap.request.RequestGeoGetNearbyCoordinate;
 import net.iGap.request.RequestGeoUpdateComment;
 import org.osmdroid.api.IMapController;
 import org.osmdroid.config.Configuration;
@@ -172,7 +177,7 @@ public class FragmentiGapMap extends Fragment implements OnLocationChanged, OnGe
             @Override
             public void onClick(View v) {
 
-                getActivity().getSupportFragmentManager().beginTransaction().remove(FragmentiGapMap.this).commit();
+                getActivity().getSupportFragmentManager().popBackStack();
                 closeKeyboard(v);
             }
         });
@@ -186,13 +191,36 @@ public class FragmentiGapMap extends Fragment implements OnLocationChanged, OnGe
             }
         });
 
-        TextView txtMessageGps = (TextView) view.findViewById(R.id.txtMessageGps);
-        txtMessageGps.setTextColor(Color.parseColor(G.appBarColor));
-        txtMessageGps.setOnClickListener(new View.OnClickListener() {
+        final TextView txtSendMessageGps = (TextView) view.findViewById(R.id.txtSendMessageGps);
+        txtSendMessageGps.setTextColor(Color.parseColor(G.appBarColor));
+        txtSendMessageGps.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 new RequestGeoUpdateComment().updateComment(edtMessageGps.getText().toString());
                 edtMessageGps.setText("");
+            }
+        });
+
+        edtMessageGps.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+                if (s.length() > 0) {
+                    txtSendMessageGps.setText(getResources().getString(R.string.md_downwards_arrow));
+                } else {
+                    txtSendMessageGps.setText(getResources().getString(R.string.md_back_arrow));
+                }
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
             }
         });
 
@@ -210,15 +238,106 @@ public class FragmentiGapMap extends Fragment implements OnLocationChanged, OnGe
         });
 
         RippleView rippleMoreMap = (RippleView) view.findViewById(R.id.ripple_more_map);
+
         rippleMoreMap.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                FragmentMapUsers fragmentMapUsers = FragmentMapUsers.newInstance();
-                try {
-                    mActivity.getSupportFragmentManager().beginTransaction().setCustomAnimations(R.anim.slide_in_right, R.anim.slide_out_left, R.anim.slide_in_right, R.anim.slide_out_left).replace(R.id.mapContainer, fragmentMapUsers).commitAllowingStateLoss();
-                } catch (Exception e) {
-                    e.getStackTrace();
-                }
+
+                final MaterialDialog dialog = new MaterialDialog.Builder(mActivity).customView(R.layout.chat_popup_dialog_custom, true).build();
+                View v = dialog.getCustomView();
+                DialogAnimation.animationUp(dialog);
+                dialog.show();
+
+
+                ViewGroup root1 = (ViewGroup) v.findViewById(R.id.dialog_root_item1_notification);
+                ViewGroup root2 = (ViewGroup) v.findViewById(R.id.dialog_root_item2_notification);
+                ViewGroup root3 = (ViewGroup) v.findViewById(R.id.dialog_root_item3_notification);
+
+                root1.setVisibility(View.VISIBLE);
+                root2.setVisibility(View.VISIBLE);
+                root3.setVisibility(View.VISIBLE);
+
+                TextView txtItem1 = (TextView) v.findViewById(R.id.dialog_text_item1_notification);
+                TextView icon1 = (TextView) v.findViewById(R.id.dialog_icon_item1_notification);
+                txtItem1.setText(getResources().getString(R.string.list_user_map));
+                icon1.setText(getResources().getString(R.string.md_delete_acc));
+
+                TextView txtItem2 = (TextView) v.findViewById(R.id.dialog_text_item2_notification);
+                TextView icon2 = (TextView) v.findViewById(R.id.dialog_icon_item2_notification);
+                txtItem2.setText(getResources().getString(R.string.turn_off_on_gps));
+                icon2.setText(getResources().getString(R.string.md_delete_acc));
+
+
+                TextView txtItem3 = (TextView) v.findViewById(R.id.dialog_text_item3_notification);
+                TextView icon3 = (TextView) v.findViewById(R.id.dialog_icon_item3_notification);
+                txtItem3.setText(getResources().getString(R.string.nearby));
+                icon3.setText(getResources().getString(R.string.md_delete_acc));
+
+
+
+                txtItem3.setText(getResources().getString(R.string.nearby));
+
+                root1.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        dialog.dismiss();
+                        FragmentMapUsers fragmentMapUsers = FragmentMapUsers.newInstance();
+                        try {
+                            mActivity.getSupportFragmentManager().beginTransaction().addToBackStack(null).setCustomAnimations(R.anim.slide_in_right, R.anim.slide_out_left, R.anim.slide_in_right, R.anim.slide_out_left).replace(R.id.replace, fragmentMapUsers).commitAllowingStateLoss();
+                        } catch (Exception e) {
+                            e.getStackTrace();
+                        }
+                    }
+                });
+
+                root2.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        dialog.dismiss();
+
+                        final MaterialDialog dialog = new MaterialDialog.Builder(mActivity).customView(R.layout.dialog_map_turnoff_gps, true).build();
+                        View v = dialog.getCustomView();
+                        DialogAnimation.animationUp(dialog);
+                        dialog.show();
+                        ToggleButton tgGpsOff = (ToggleButton) v.findViewById(R.id.toggleGpsOff);
+                        TextView txtOffOnGps = (TextView) v.findViewById(R.id.txtOffOnGps);
+                        TextView txtIconTurnOnOrOff = (TextView) v.findViewById(R.id.txtIconTurnOnOrOff);
+
+                        final LocationManager manager = (LocationManager) mActivity.getSystemService(Context.LOCATION_SERVICE);
+
+                        if (!manager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+                            tgGpsOff.setChecked(false);
+                            txtOffOnGps.setText(getResources().getString(R.string.turn_on_gps));
+                            txtIconTurnOnOrOff.setText(getResources().getString(R.string.md_visibility));
+
+                        } else {
+                            tgGpsOff.setChecked(true);
+                            txtOffOnGps.setText(getResources().getString(R.string.turn_off_gps));
+                            txtIconTurnOnOrOff.setText(getResources().getString(R.string.md_gap_eye_off));
+                        }
+
+
+                        tgGpsOff.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+
+                                dialog.dismiss();
+                                startActivity(new Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS));
+                            }
+                        });
+                    }
+                });
+
+                root3.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        dialog.dismiss();
+                        if (location != null) {
+                            new RequestGeoGetNearbyCoordinate().getNearbyCoordinate(location.getLatitude(), location.getLongitude());
+                        }
+                    }
+                });
+
             }
         });
 
