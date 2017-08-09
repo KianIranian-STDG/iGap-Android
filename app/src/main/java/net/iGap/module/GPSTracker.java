11a20a69ce11aa2abaa8328852f832243827affa
Amir Molareza <molareza@rooyekhat.co>
2017-08-09 16:44:31 +0430

    some change on map and string

--- app/src/main/java/net/iGap/module/GPSTracker.java
+++ app/src/main/java/net/iGap/module/GPSTracker.java
@@ -70,7 +70,23 @@ public class GPSTracker extends Service implements LocationListener {
                 // no network provider is enabled
             } else {
                 this.canGetLocation = true;
-                if (isNetworkEnabled) {
+                // if GPS Enabled get lat/long using GPS Services
+                if (isGPSEnabled) {
+                    if (location == null) {
+                        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, MIN_TIME_BW_UPDATES, MIN_DISTANCE_CHANGE_FOR_UPDATES, this);
+                        if (locationManager != null) {
+                            location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                            if (location != null) {
+                                latitude = location.getLatitude();
+                                longitude = location.getLongitude();
+                                if (G.onLocationChanged != null) {
+                                    G.onLocationChanged.onLocationChanged(location);
+                                }
+                            }
+                        }
+                    }
+                } else if (isNetworkEnabled) {
+
                     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
                         if (context.checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && context.checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
                             return;
@@ -84,22 +100,6 @@ public class GPSTracker extends Service implements LocationListener {
                             longitude = location.getLongitude();
                             if (G.onLocationChanged != null) {
                                 G.onLocationChanged.onLocationChanged(location);
-                            }
-                        }
-                    }
-                }
-                // if GPS Enabled get lat/long using GPS Services
-                if (isGPSEnabled) {
-                    if (location == null) {
-                        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, MIN_TIME_BW_UPDATES, MIN_DISTANCE_CHANGE_FOR_UPDATES, this);
-                        if (locationManager != null) {
-                            location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-                            if (location != null) {
-                                latitude = location.getLatitude();
-                                longitude = location.getLongitude();
-                                if (G.onLocationChanged != null) {
-                                    G.onLocationChanged.onLocationChanged(location);
-                                }
                             }
                         }
                     }
