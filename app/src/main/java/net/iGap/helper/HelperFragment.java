500fdfc0e006c6a16fda6be54e8e46df36acd76d
Saeed Mozaffari <mozafari@rooyekhat.co>
2017-08-30 10:43:44 +0430

    Show Fragment InFrontOf Drawer

--- app/src/main/java/net/iGap/helper/HelperFragment.java
+++ app/src/main/java/net/iGap/helper/HelperFragment.java
@@ -196,7 +196,7 @@ public class HelperFragment {
                 }
             }
         } else {
-            resId = R.id.am_frame_main_container;
+            resId = R.id.frame_main;
         }
 
         return resId;
