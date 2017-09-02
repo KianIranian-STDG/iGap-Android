833ee86bbfb7ff386f00fe55a59177ad6ba6d553
Saeed Mozaffari <mozafari@rooyekhat.co>
2017-09-02 18:10:03 +0430

    Simple Change

--- app/src/main/java/net/iGap/fragments/FragmentChat.java
+++ app/src/main/java/net/iGap/fragments/FragmentChat.java
@@ -7751,7 +7751,7 @@ public class FragmentChat extends BaseFragment
     private void closeKeyboard(View v) {
         if (isAdded()) {
             try {
-                InputMethodManager imm = (InputMethodManager) mActivity.getSystemService(Context.INPUT_METHOD_SERVICE);
+                InputMethodManager imm = (InputMethodManager) G.context.getSystemService(Context.INPUT_METHOD_SERVICE);
                 imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
             } catch (IllegalStateException e) {
                 e.getStackTrace();
