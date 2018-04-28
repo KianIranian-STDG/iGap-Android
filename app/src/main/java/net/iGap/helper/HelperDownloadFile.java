96fcaba2955b8d2723957d2d960045131a8a6b40
android3 <nejati@rooyekhat.co>
2018-04-28 16:07:02 +0430

    fix Helper download

--- app/src/main/java/net/iGap/helper/HelperDownloadFile.java
+++ app/src/main/java/net/iGap/helper/HelperDownloadFile.java
@@ -48,7 +48,7 @@ public class HelperDownloadFile {
     private static ArrayList<StructQueue> mQueue = new ArrayList<>();
     private static int maxDownloadSize = 4;
     private OnFileDownloadResponse onFileDownloadResponse;
-    private static int downloadId;
+
 
     public HelperDownloadFile() {
 
@@ -385,7 +385,7 @@ public class HelperDownloadFile {
             updateView(item);
             list.remove(item.cashId + item.selector);
 //            if (item.selector == ProtoFileDownload.FileDownload.Selector.FILE) {
-//                addDownloadFromQueue();
+            addDownloadFromQueue();
 //            }
 
             // save downloaded file to gallery
