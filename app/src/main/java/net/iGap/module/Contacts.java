bd62bcacfa12038962c5923dce6e5d8aef68a286
Amir Molareza <molareza@rooyekhat.co>
2018-05-09 12:01:07 +0430

    fix name on contact

--- app/src/main/java/net/iGap/module/Contacts.java
+++ app/src/main/java/net/iGap/module/Contacts.java
@@ -1,12 +1,12 @@
 /*
-* This is the source code of iGap for Android
-* It is licensed under GNU AGPL v3.0
-* You should have received a copy of the license in this archive (see LICENSE).
-* Copyright © 2017 , iGap - www.iGap.net
-* iGap Messenger | Free, Fast and Secure instant messaging application
-* The idea of the RooyeKhat Media Company - www.RooyeKhat.co
-* All rights reserved.
-*/
+ * This is the source code of iGap for Android
+ * It is licensed under GNU AGPL v3.0
+ * You should have received a copy of the license in this archive (see LICENSE).
+ * Copyright © 2017 , iGap - www.iGap.net
+ * iGap Messenger | Free, Fast and Secure instant messaging application
+ * The idea of the RooyeKhat Media Company - www.RooyeKhat.co
+ * All rights reserved.
+ */
 
 package net.iGap.module;
 
@@ -194,7 +194,7 @@ public class Contacts {
                         itemContact.setDisplayName(contactList.get(i).displayName);
                     } else if (sp.length == 3) {
                         itemContact.setFirstName(sp[0]);
-                        itemContact.setLastName(sp[1] + sp[2]);
+                        itemContact.setLastName(sp[1] + " " + sp[2]);
                         itemContact.setPhone(contactList.get(i).getPhone());
                         itemContact.setDisplayName(contactList.get(i).displayName);
                     } else if (sp.length >= 3) {
