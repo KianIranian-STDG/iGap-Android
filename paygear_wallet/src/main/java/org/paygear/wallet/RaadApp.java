497674868299607d37ff3ed4913d0080d77f6118
android3 <nejati@rooyekhat.co>
2018-07-07 11:59:34 +0430

    add lib

---
+++ paygear_wallet/src/main/java/org/paygear/wallet/RaadApp.java
@@ -0,0 +1,64 @@
+package org.paygear.wallet;
+
+import android.app.Application;
+import android.content.Context;
+import org.paygear.wallet.model.Card;
+import org.paygear.wallet.web.Web;
+
+import java.util.ArrayList;
+
+import ir.radsense.raadcore.OnWebResponseListener;
+import ir.radsense.raadcore.Raad;
+import ir.radsense.raadcore.model.Account;
+import ir.radsense.raadcore.utils.RaadCommonUtils;
+import ir.radsense.raadcore.web.WebBase;
+import retrofit2.Response;
+
+/**
+ * Created by Software1 on 8/29/2017.
+ */
+
+public class RaadApp extends Application {
+
+    public static final int NOTIFICATION_TYPE_LIKE = 1;
+    public static final int NOTIFICATION_TYPE_COMMENT = 2;
+    public static final int NOTIFICATION_TYPE_NEW_MESSAGE = 3;
+    public static final int NOTIFICATION_TYPE_PAY_COMPLETE = 4;
+    public static final int NOTIFICATION_TYPE_COUPON = 5;
+    public static final int NOTIFICATION_TYPE_DELIVERY = 6;
+    public static final int NOTIFICATION_TYPE_FOLLOW = 7;
+    public static final int NOTIFICATION_TYPE_CLUB = 9;
+
+    public static final String BROADCAST_INTENT_NEW_MESSAGE = "NEW_MESSAGE";
+
+    public static String appVersion;
+
+    public static Account me;
+    public static Card paygearCard;
+    public static ArrayList<Card> cards;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        WebBase.apiKey = Web.API_KEY;
+        WebBase.isDebug = true;
+        WebBase.onResponseListener = new OnWebResponseListener() {
+            @Override
+            public boolean onResponse(Context context, Response response) {
+                /*if (context instanceof NavigationBarActivity) {
+                    if (response.code() == 401 || response.code() == 403) {
+                        Utils.signOutAndGoLogin((NavigationBarActivity) context);
+                        return false;
+                    }
+                }*/
+                return true;
+            }
+        };
+
+        Raad.init(getApplicationContext());
+
+        appVersion = RaadCommonUtils.getAppVersionName(getApplicationContext());
+        //Raad.deviceToken = SettingHelper.getString(getApplicationContext(), SettingHelper.DEVICE_TOKEN, null);
+    }
+
+}
---
+++ paygear_wallet/src/main/java/org/paygear/wallet/WalletActivity.java
@@ -0,0 +1,95 @@
+package org.paygear.wallet;
+
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v4.app.FragmentTransaction;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.RelativeLayout;
+
+import org.paygear.wallet.fragment.CardsFragment;
+import org.paygear.wallet.fragment.CashOutFragment;
+import org.paygear.wallet.fragment.CashOutRequestFragment;
+import org.paygear.wallet.model.Payment;
+import org.paygear.wallet.model.PaymentAuth;
+import org.paygear.wallet.utils.Utils;
+import org.paygear.wallet.web.Web;
+
+import ir.radsense.raadcore.OnWebResponseListener;
+import ir.radsense.raadcore.Raad;
+import ir.radsense.raadcore.app.NavigationBarActivity;
+import ir.radsense.raadcore.web.WebBase;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class WalletActivity extends NavigationBarActivity {
+
+    public static String token;
+    public static Intent intent;
+    public static String primaryColor="#f69228";
+    public static String darkPrimaryColor="#37a9a1";
+    public static String accentColor="00B0Bf";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        RelativeLayout navBarView = findViewById(R.id.nav_bar);
+        navBarView.setVisibility(View.GONE);
+
+
+        WebBase.apiKey = Web.API_KEY;
+        WebBase.isDebug = true;
+        WebBase.onResponseListener = new OnWebResponseListener() {
+            @Override
+            public boolean onResponse(Context context, Response response) {
+                /*if (context instanceof NavigationBarActivity) {
+                    if (response.code() == 401 || response.code() == 403) {
+                        Utils.signOutAndGoLogin((NavigationBarActivity) context);
+                        return false;
+                    }
+                }*/
+                return true;
+            }
+        };
+
+        Raad.init(getApplicationContext());
+        intent=getIntent();
+        String phone = intent.getStringExtra("Mobile");
+        String language = intent.getStringExtra("Language");
+        boolean isP2P = intent.getBooleanExtra("IsP2P", false);
+        Payment payment=(Payment) intent.getSerializableExtra("Payment");
+        primaryColor=intent.getStringExtra("PrimaryColor");
+        darkPrimaryColor=intent.getStringExtra("DarkPrimaryColor");
+        accentColor=intent.getStringExtra("AccentColor");
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Window window = getWindow();
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.setStatusBarColor(Color.parseColor(WalletActivity.darkPrimaryColor));
+        }
+        if (language != null) {
+            Utils.setLocale(this, language);
+        }
+
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+
+        if (isP2P && payment != null) {
+            ft.add(R.id.content_container,  CardsFragment.newInstance(payment));
+
+        } else {
+            ft.add(R.id.content_container, new CardsFragment());
+        }
+        ft.commit();
+    }
+    
+    @Override
+    protected boolean isNavBarShowingOnSwitchFragment(String fragmentName) {
+        return false;
+    }
+}
