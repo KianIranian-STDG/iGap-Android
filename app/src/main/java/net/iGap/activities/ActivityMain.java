d30728c0a313f59b545d45a062892693841050b6
Amir Molareza <molareza@rooyekhat.co>
2018-08-19 12:55:31 +0430

    fix nullPointException SelectedLanguage

--- app/src/main/java/net/iGap/activities/ActivityMain.java
+++ app/src/main/java/net/iGap/activities/ActivityMain.java
@@ -2076,24 +2076,17 @@ public class ActivityMain extends ActivityEnhanced implements OnUserInfoMyClient
 
     private void connectionState() {
         final TextView txtIgap = (TextView) findViewById(R.id.cl_txt_igap);
-
-        Typeface typeface = null;
-        if (G.selectedLanguage.equals("fa") || G.selectedLanguage.equals("ar")) {
-            typeface = titleTypeface;
-        }
+        
         if (G.connectionState == ConnectionState.WAITING_FOR_NETWORK) {
             txtIgap.setText(R.string.waiting_for_network);
-            txtIgap.setTypeface(typeface, Typeface.BOLD);
         } else if (G.connectionState == ConnectionState.CONNECTING) {
             txtIgap.setText(R.string.connecting);
-            txtIgap.setTypeface(typeface, Typeface.BOLD);
         } else if (G.connectionState == ConnectionState.UPDATING) {
             txtIgap.setText(updating);
-            txtIgap.setTypeface(typeface, Typeface.BOLD);
         } else {
             txtIgap.setText(R.string.app_name);
-            txtIgap.setTypeface(titleTypeface, Typeface.BOLD);
         }
+        txtIgap.setTypeface(titleTypeface, Typeface.BOLD);
 
         G.onConnectionChangeState = new OnConnectionChangeState() {
             @Override
