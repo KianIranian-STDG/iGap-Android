35a5f2592d133c24273f700a17ae427b1e7bdeb0
m.nejati <mehdi_nejati24@yahoo.com>
2019-02-23 03:59:27 -0800

    Auto fill sms

---
+++ app/src/main/java/net/iGap/module/SmsRetriver/SMSReceiver.java
@@ -0,0 +1,73 @@
+package net.iGap.module.SmsRetriver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.google.android.gms.auth.api.phone.SmsRetriever;
+import com.google.android.gms.common.api.CommonStatusCodes;
+import com.google.android.gms.common.api.Status;
+
+public class SMSReceiver extends BroadcastReceiver {
+
+    private OTPReceiveListener otpListener;
+
+    /**
+     * @param otpListener
+     */
+    public void setOTPListener(OTPReceiveListener otpListener) {
+        this.otpListener = otpListener;
+    }
+
+
+    /**
+     * @param context
+     * @param intent
+     */
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if (SmsRetriever.SMS_RETRIEVED_ACTION.equals(intent.getAction())) {
+            Bundle extras = intent.getExtras();
+            Status status = (Status) extras.get(SmsRetriever.EXTRA_STATUS);
+
+            switch (status.getStatusCode()) {
+                case CommonStatusCodes.SUCCESS:
+                    String message = (String) extras.get(SmsRetriever.EXTRA_SMS_MESSAGE);
+                    if (otpListener != null) {
+                        otpListener.onOTPReceived(message);
+                    }
+                    break;
+                case CommonStatusCodes.TIMEOUT:
+                    if (otpListener != null) {
+                        otpListener.onOTPTimeOut();
+                    }
+                    break;
+                case CommonStatusCodes.API_NOT_CONNECTED:
+                    if (otpListener != null) {
+                        otpListener.onOTPReceivedError("API NOT CONNECTED");
+                    }
+                    break;
+                case CommonStatusCodes.NETWORK_ERROR:
+                    if (otpListener != null) {
+                        otpListener.onOTPReceivedError("NETWORK ERROR");
+                    }
+                    break;
+                case CommonStatusCodes.ERROR:
+                    if (otpListener != null) {
+                        otpListener.onOTPReceivedError("SOME THING WENT WRONG");
+                    }
+                    break;
+            }
+        }
+    }
+
+    public interface OTPReceiveListener {
+
+        void onOTPReceived(String otp);
+
+        void onOTPTimeOut();
+
+        void onOTPReceivedError(String error);
+    }
+}
