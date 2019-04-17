64244f74948646e2d8ad4a092ca94fa8a4a3ec0d
aabolfazl <a.abbasi@kianiranian.com>
2019-04-17 19:42:13 +0430

    Check out customer club

--- app/src/main/java/net/iGap/fragments/FragmentCustomerClubProfile.java
+++ app/src/main/java/net/iGap/fragments/FragmentCustomerClubProfile.java
@@ -1,5 +1,6 @@
 package net.iGap.fragments;
 
+import android.content.Intent;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -8,14 +9,22 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import net.iGap.G;
 import net.iGap.R;
 import net.iGap.databinding.FragmentCustomrtclubProfileBinding;
+import net.iGap.helper.HelperAvatar;
+import net.iGap.helper.HelperImageBackColor;
+import net.iGap.interfaces.OnAvatarGet;
+import net.iGap.module.AndroidUtils;
+import net.iGap.module.CircleImageView;
 import net.iGap.viewmodel.CustomerClubViewModel;
 
 public class FragmentCustomerClubProfile extends BaseFragment {
 
     private FragmentCustomrtclubProfileBinding binding;
     private CustomerClubViewModel viewModel;
+    private CircleImageView profileImage;
+
 
     @Nullable
     @Override
@@ -33,12 +42,54 @@ public class FragmentCustomerClubProfile extends BaseFragment {
     private void setUpViews() {
         viewModel = new CustomerClubViewModel(this);
         binding.setViewModel(viewModel);
+        profileImage = binding.ivCustomerClubProfile;
+
     }
 
     @Override
     public void onStart() {
         super.onStart();
 
+        setImage();
 
+    }
+
+    private void setImage() {
+        HelperAvatar.getAvatar(G.userId, HelperAvatar.AvatarType.USER, true, new OnAvatarGet() {
+            @Override
+            public void onAvatarGet(final String avatarPath, long ownerId) {
+                if (avatarPath != null) {
+                    G.handler.post(() -> {
+                        if (G.userId != ownerId)
+                            return;
+                        G.imageLoader.displayImage(AndroidUtils.suitablePath(avatarPath), profileImage);
+                    });
+                }
+            }
+
+            @Override
+            public void onShowInitials(final String initials, final String color, final long ownerId) {
+                G.handler.post(() -> {
+                    if (G.userId != ownerId)
+                        return;
+                    profileImage.setImageBitmap(HelperImageBackColor.drawAlphabetOnPicture((int) profileImage.getContext().getResources().getDimension(R.dimen.dp100), initials, color));
+                });
+            }
+        });
+
+
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        viewModel.onDestroy();
+    }
+
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        viewModel.onResult(requestCode, resultCode, data);
     }
 }
--- app/src/main/java/net/iGap/viewmodel/CustomerClubViewModel.java
+++ app/src/main/java/net/iGap/viewmodel/CustomerClubViewModel.java
@@ -1,15 +1,64 @@
 package net.iGap.viewmodel;
 
-import android.arch.lifecycle.ViewModel;
+import android.content.Intent;
 import android.databinding.ObservableField;
+import android.widget.Toast;
 
+import com.google.zxing.integration.android.IntentIntegrator;
+
+import net.iGap.G;
+import net.iGap.R;
 import net.iGap.fragments.FragmentCustomerClubProfile;
+import net.iGap.realm.RealmUserInfo;
 
-public class CustomerClubViewModel extends ViewModel {
-    public ObservableField<String> profileName = new ObservableField<>("سلام دیتابایندینگ :)");
-    private FragmentCustomerClubProfile fragmentChannelProfile;
+import io.realm.Realm;
 
-    public CustomerClubViewModel(FragmentCustomerClubProfile fragmentChannelProfile) {
-        this.fragmentChannelProfile = fragmentChannelProfile;
+public class CustomerClubViewModel {
+    public static final int requestCodeQrCode = 200;
+    public ObservableField<String> profileNameTv = new ObservableField<>("");
+    public ObservableField<String> referralTv = new ObservableField<>("0");
+    public ObservableField<String> pointsTv = new ObservableField<>("300");
+    private RealmUserInfo realmUserInfo;
+    private Realm mRealm;
+    private FragmentCustomerClubProfile fragmentCustomerClubProfile;
+
+
+    public CustomerClubViewModel(FragmentCustomerClubProfile fragmentCustomerClubProfile) {
+        this.fragmentCustomerClubProfile = fragmentCustomerClubProfile;
+        initData();
     }
+
+    private void initData() {
+        realmUserInfo = getRealm().where(RealmUserInfo.class).findFirst();
+        if (getRealm() != null) {
+            profileNameTv.set(realmUserInfo.getUserInfo().getDisplayName());
+            referralTv.set(G.context.getString(R.string.ra_title) + " " + realmUserInfo.getRepresentPhoneNumber());
+        }
+    }
+
+    private Realm getRealm() {
+        if (mRealm == null || mRealm.isClosed()) {
+            mRealm = Realm.getDefaultInstance();
+        }
+        return mRealm;
+    }
+
+    public void onOrderHistoryClick() {
+        IntentIntegrator integrator = new IntentIntegrator(fragmentCustomerClubProfile.getActivity());
+        integrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE);
+        integrator.setRequestCode(requestCodeQrCode);
+        integrator.setBeepEnabled(false);
+        integrator.setPrompt("");
+        integrator.initiateScan();
+    }
+
+    public void onDestroy() {
+        mRealm.close();
+    }
+
+    public void onResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == requestCodeQrCode)
+            Toast.makeText(fragmentCustomerClubProfile.getContext(), data + "", Toast.LENGTH_SHORT).show();
+    }
+
 }
