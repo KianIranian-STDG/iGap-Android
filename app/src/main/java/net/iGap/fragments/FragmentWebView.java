61ae896078d732c963bccc3452e9b703b9c0eb07
Mehdi.Bagvand <m.bagvand@kianiranian.com>
2019-04-17 12:42:29 +0430

    improvement and fix

--- app/src/main/java/net/iGap/fragments/FragmentWebView.java
+++ app/src/main/java/net/iGap/fragments/FragmentWebView.java
@@ -28,7 +28,8 @@ public class FragmentWebView extends FragmentToolBarBack implements IOnBackPress
     private ProgressBar progressWebView;
     private TextView webViewError;
     Handler delayHandler = new Handler();
-    Runnable taskMakeGoneWebViewWithDelay;
+    Runnable taskMakeVisibleWebViewWithDelay;
+    CustomWebViewClient customWebViewClient;
 
     public static FragmentWebView newInstance(String url) {
         FragmentWebView discoveryFragment = new FragmentWebView();
@@ -62,27 +63,13 @@ public class FragmentWebView extends FragmentToolBarBack implements IOnBackPress
             @Override
             public void onClick(View v) {
                 progressWebView.setVisibility(View.VISIBLE);
-                setUpWebView();
+                customWebViewClient.isWebViewVisible = true;
+                webView.clearView();
+                webView.reload();
                 setWebViewVisibleWithDelay();
             }
         });
 
-        setUpWebView();
-    }
-
-    private void setWebViewVisibleWithDelay() {
-        delayHandler.removeCallbacks(taskMakeGoneWebViewWithDelay);
-        taskMakeGoneWebViewWithDelay = new Runnable() {
-            @Override
-            public void run() {
-                webViewError.setVisibility(View.GONE);
-                webView.setVisibility(View.VISIBLE);
-            }
-        };
-        delayHandler.postDelayed(taskMakeGoneWebViewWithDelay, 500);
-    }
-
-    private void setUpWebView() {
         titleTextView.setText(G.context.getString(R.string.igap));
         webView.getSettings().setLoadsImagesAutomatically(true);
         webView.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
@@ -94,7 +81,6 @@ public class FragmentWebView extends FragmentToolBarBack implements IOnBackPress
         webView.getSettings().setJavaScriptEnabled(true);
         webView.getSettings().setDomStorageEnabled(true);
         webView.setWebChromeClient(new WebChromeClient() {
-
             @Override
             public void onProgressChanged(WebView view, int progress) {
                 if (progress == 100) {
@@ -104,54 +90,22 @@ public class FragmentWebView extends FragmentToolBarBack implements IOnBackPress
                 }
             }
         });
-
-        webView.setWebViewClient(new MyWebViewClient() {
-            private boolean isWebViewVisible = true;
-            @Override
-            protected void onReceivedError(WebView webView, String url, int errorCode, String description) {
-                if (url.equals(FragmentWebView.this.url) && isWebViewVisible) {
-                    isWebViewVisible = false;
-                    delayHandler.removeCallbacks(taskMakeGoneWebViewWithDelay);
-                    webViewError.setVisibility(View.VISIBLE);
-                    webView.setVisibility(View.GONE);
-                    titleTextView.setText(G.context.getString(R.string.igap));
-                    HelperError.showSnackMessage(getString(R.string.wallet_error_server), false);
-                }
-            }
-
-            @Override
-            protected boolean handleUri(WebView webView, Uri uri) {
-                final String host = uri.getHost();
-                final String scheme = uri.getScheme();
-                // Returning false means that you are going to load this url in the webView itself
-                // Returning true means that you need to handle what to do with the url e.g. open web page in a Browser
-
-                // final Intent intent = new Intent(Intent.ACTION_VIEW, uri);
-                // startActivity(intent);
-                return false;
-
-            }
-
-            @Override
-            public void onPageStarted(WebView view, String url, Bitmap favicon) {
-                super.onPageStarted(view, url, favicon);
-
-            }
-
-            @Override
-            public void onPageFinished(WebView view, String url) {
-                super.onPageFinished(view, url);
-                if (isWebViewVisible) {
-                    titleTextView.setText(view.getTitle());
-                }
-
-            }
-        });
-
+        customWebViewClient = new CustomWebViewClient();
+        webView.setWebViewClient(customWebViewClient);
         webView.loadUrl(url);
-
     }
 
+    private void setWebViewVisibleWithDelay() {
+        delayHandler.removeCallbacks(taskMakeVisibleWebViewWithDelay);
+        taskMakeVisibleWebViewWithDelay = new Runnable() {
+            @Override
+            public void run() {
+                webViewError.setVisibility(View.GONE);
+                webView.setVisibility(View.VISIBLE);
+            }
+        };
+        delayHandler.postDelayed(taskMakeVisibleWebViewWithDelay, 500);
+    }
 
     @Override
     public void onStart() {
@@ -169,7 +123,10 @@ public class FragmentWebView extends FragmentToolBarBack implements IOnBackPress
     public boolean onBack() {
         webView.stopLoading();
         if (webView.canGoBack()) {
+            webView.clearView();
             webView.goBack();
+            customWebViewClient.isWebViewVisible = true;
+            setWebViewVisibleWithDelay();
             return true;
         }
         return false;
@@ -179,9 +136,58 @@ public class FragmentWebView extends FragmentToolBarBack implements IOnBackPress
     protected void onBackButtonClicked(View view) {
         webView.stopLoading();
         if (webView.canGoBack()) {
+            webView.clearView();
             webView.goBack();
+            customWebViewClient.isWebViewVisible = true;
+            setWebViewVisibleWithDelay();
         } else {
             super.onBackButtonClicked(view);
+        }
+    }
+
+    private class CustomWebViewClient extends MyWebViewClient {
+
+        public boolean isWebViewVisible = true;
+
+        @Override
+        protected void onReceivedError(WebView webView, String url, int errorCode, String description) {
+//            if (url.equals(FragmentWebView.this.url) && isWebViewVisible) {
+            if (isWebViewVisible) {
+                isWebViewVisible = false;
+                delayHandler.removeCallbacks(taskMakeVisibleWebViewWithDelay);
+                webViewError.setVisibility(View.VISIBLE);
+                webView.setVisibility(View.GONE);
+                titleTextView.setText(G.context.getString(R.string.igap));
+                HelperError.showSnackMessage(getString(R.string.wallet_error_server), false);
+            }
+        }
+
+        @Override
+        protected boolean handleUri(WebView webView, Uri uri) {
+            final String host = uri.getHost();
+            final String scheme = uri.getScheme();
+            // Returning false means that you are going to load this url in the webView itself
+            // Returning true means that you need to handle what to do with the url e.g. open web page in a Browser
+
+            // final Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+            // startActivity(intent);
+            return false;
+
+        }
+
+        @Override
+        public void onPageStarted(WebView view, String url, Bitmap favicon) {
+            super.onPageStarted(view, url, favicon);
+
+        }
+
+        @Override
+        public void onPageFinished(WebView view, String url) {
+            super.onPageFinished(view, url);
+            if (isWebViewVisible && !view.getTitle().contains("صفحه وب در دسترس")) {
+                titleTextView.setText(view.getTitle());
+            }
+
         }
     }
 }
