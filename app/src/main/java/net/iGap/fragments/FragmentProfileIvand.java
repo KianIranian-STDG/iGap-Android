de19e597485cc18fae5342fc190219dc7f5e57cf
a.nazari <a.nazari@kianiranian.com>
2019-05-06 13:59:46 +0430

    complete ivand (discovery) user profile ui

---
+++ app/src/main/java/net/iGap/fragments/FragmentProfileIvand.java
@@ -0,0 +1,26 @@
+package net.iGap.fragments;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import net.iGap.R;
+
+
+public class FragmentProfileIvand extends Fragment {
+
+
+    public FragmentProfileIvand() {
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.fragment_profile_ivand, container, false);
+    }
+
+}
