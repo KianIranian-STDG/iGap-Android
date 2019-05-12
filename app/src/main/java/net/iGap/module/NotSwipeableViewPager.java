f7ff5a17665488337208b2864879bf97fd8c734d
a.nazari <a.nazari@kianiranian.com>
2019-05-12 13:17:00 +0430

    disable main activity view pager swipe

---
+++ app/src/main/java/net/iGap/module/NotSwipeableViewPager.java
@@ -0,0 +1,45 @@
+package net.iGap.module;
+
+
+import android.content.Context;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+
+/**
+ * @author Alireza Nazari
+ * @address http://alirezanazari.ir
+ * @since 1396/5/10
+ * @version 1.0.0
+ */
+public class NotSwipeableViewPager extends ViewPager {
+
+    private boolean enabled;
+
+    public NotSwipeableViewPager(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        this.enabled = true;
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if (this.enabled) {
+            return super.onTouchEvent(event);
+        }
+
+        return false;
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent event) {
+        if (this.enabled) {
+            return super.onInterceptTouchEvent(event);
+        }
+
+        return false;
+    }
+
+    public void setPagingEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+}
