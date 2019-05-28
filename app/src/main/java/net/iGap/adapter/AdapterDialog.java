d9b9da98f4d65dc232f0375ed03d928d8e9b4626
a.zarghalami <a.zarghalami@kian.local>
2019-05-28 12:35:29 +0430

    remove country flag from choose country dialog

--- app/src/main/java/net/iGap/adapter/AdapterDialog.java
+++ app/src/main/java/net/iGap/adapter/AdapterDialog.java
@@ -1,12 +1,12 @@
 /*
-* This is the source code of iGap for Android
-* It is licensed under GNU AGPL v3.0
-* You should have received a copy of the license in this archive (see LICENSE).
-* Copyright © 2017 , iGap - www.iGap.net
-* iGap Messenger | Free, Fast and Secure instant messaging application
-* The idea of the Kianiranian Company - www.kianiranian.com
-* All rights reserved.
-*/
+ * This is the source code of iGap for Android
+ * It is licensed under GNU AGPL v3.0
+ * You should have received a copy of the license in this archive (see LICENSE).
+ * Copyright © 2017 , iGap - www.iGap.net
+ * iGap Messenger | Free, Fast and Secure instant messaging application
+ * The idea of the Kianiranian Company - www.kianiranian.com
+ * All rights reserved.
+ */
 
 package net.iGap.adapter;
 
@@ -18,6 +18,7 @@ import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.Filter;
 import android.widget.Filterable;
+
 import net.iGap.R;
 import net.iGap.module.CircleImageView;
 import net.iGap.module.structs.StructCountry;
@@ -75,6 +76,7 @@ public class AdapterDialog extends BaseAdapter implements Filterable {
         viewHolder.countryCode.setText(structCountry.getCountryCode());
         viewHolder.countryName.setText(structCountry.getName());
         viewHolder.countyImage.setImageResource(R.mipmap.icon_rounded/*structCountry.getFlag()*/);
+        viewHolder.countyImage.setVisibility(View.INVISIBLE);
         return convertView;
     }
 
@@ -121,7 +123,7 @@ public class AdapterDialog extends BaseAdapter implements Filterable {
 
     private class ViewHolder {
         private CircleImageView countyImage;
-        private AppCompatTextView countryName,countryCode;
+        private AppCompatTextView countryName, countryCode;
     }
 
 }
