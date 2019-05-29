7805dd06d179563e3560da978e34bb65f62d9f5a
aabolfazl <a.abbasi@kianiranian.com>
2019-05-29 16:44:47 +0430

    change contact item

--- app/src/main/java/net/iGap/adapter/items/chat/ContactItem.java
+++ app/src/main/java/net/iGap/adapter/items/chat/ContactItem.java
@@ -1,40 +1,33 @@
 /*
-* This is the source code of iGap for Android
-* It is licensed under GNU AGPL v3.0
-* You should have received a copy of the license in this archive (see LICENSE).
-* Copyright © 2017 , iGap - www.iGap.net
-* iGap Messenger | Free, Fast and Secure instant messaging application
-* The idea of the Kianiranian Company - www.kianiranian.com
-* All rights reserved.
-*/
+ * This is the source code of iGap for Android
+ * It is licensed under GNU AGPL v3.0
+ * You should have received a copy of the license in this archive (see LICENSE).
+ * Copyright © 2017 , iGap - www.iGap.net
+ * iGap Messenger | Free, Fast and Secure instant messaging application
+ * The idea of the Kianiranian Company - www.kianiranian.com
+ * All rights reserved.
+ */
 
 package net.iGap.adapter.items.chat;
 
 import android.graphics.Color;
+import android.support.constraint.ConstraintLayout;
+import android.support.constraint.ConstraintSet;
 import android.support.v7.widget.AppCompatImageView;
 import android.support.v7.widget.AppCompatTextView;
-import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.TextView;
+import android.widget.Button;
 
 import net.iGap.G;
 import net.iGap.R;
 import net.iGap.adapter.MessagesAdapter;
+import net.iGap.helper.LayoutCreator;
 import net.iGap.interfaces.IMessageItem;
 import net.iGap.module.AppUtils;
 import net.iGap.proto.ProtoGlobal;
 
 import java.util.List;
-
-import io.realm.Realm;
-
-import static android.widget.LinearLayout.HORIZONTAL;
-import static android.widget.LinearLayout.VERTICAL;
-import static net.iGap.G.context;
-import static net.iGap.R.dimen.messageContainerPadding;
 
 public class ContactItem extends AbstractMessage<ContactItem, ContactItem.ViewHolder> {
 
@@ -45,15 +38,15 @@ public class ContactItem extends AbstractMessage<ContactItem, ContactItem.ViewHo
     @Override
     protected void updateLayoutForSend(ViewHolder holder) {
         super.updateLayoutForSend(holder);
-        AppUtils.setImageDrawable(holder.image, R.drawable.black_contact);
+        AppUtils.setImageDrawable(holder.contactImage, R.drawable.black_contact);
     }
 
     @Override
     protected void updateLayoutForReceive(ViewHolder holder) {
         super.updateLayoutForReceive(holder);
-        holder.name.setTextColor(holder.itemView.getResources().getColor(R.color.colorOldBlack));
-        holder.name.setTextColor(holder.itemView.getResources().getColor(R.color.colorOldBlack));
-        AppUtils.setImageDrawable(holder.image, R.drawable.green_contact);
+        holder.contactName.setTextColor(holder.itemView.getResources().getColor(R.color.colorOldBlack));
+        holder.contactName.setTextColor(holder.itemView.getResources().getColor(R.color.colorOldBlack));
+        AppUtils.setImageDrawable(holder.contactImage, R.drawable.green_contact);
     }
 
     @Override
@@ -72,13 +65,13 @@ public class ContactItem extends AbstractMessage<ContactItem, ContactItem.ViewHo
 
         if (mMessage.forwardedFrom != null) {
             if (mMessage.forwardedFrom.getRoomMessageContact() != null) {
-                holder.name.setText(mMessage.forwardedFrom.getRoomMessageContact().getFirstName() + " " + mMessage.forwardedFrom.getRoomMessageContact().getLastName());
-                holder.number.setText(mMessage.forwardedFrom.getRoomMessageContact().getLastPhoneNumber());
+                holder.contactName.setText(mMessage.forwardedFrom.getRoomMessageContact().getFirstName() + " " + mMessage.forwardedFrom.getRoomMessageContact().getLastName());
+                holder.contactNumber.setText(mMessage.forwardedFrom.getRoomMessageContact().getLastPhoneNumber());
             }
         } else {
             if (mMessage.userInfo != null) {
-                holder.name.setText(mMessage.userInfo.displayName);
-                holder.number.setText(mMessage.userInfo.phone);
+                holder.contactName.setText(mMessage.userInfo.displayName);
+                holder.contactNumber.setText(mMessage.userInfo.phone);
             }
         }
     }
@@ -88,59 +81,105 @@ public class ContactItem extends AbstractMessage<ContactItem, ContactItem.ViewHo
         return new ViewHolder(v);
     }
 
-    protected static class ViewHolder extends ChatItemHolder {
+    protected static class ViewHolder extends NewChatItemHolder {
 
-        protected AppCompatTextView name;
-        protected AppCompatTextView number;
-        protected AppCompatImageView image;
+        private AppCompatTextView contactName;
+        private AppCompatTextView contactNumber;
+        private AppCompatImageView contactImage;
+        private ConstraintLayout rootView;
+        private ConstraintSet set;
+        private Button viewContactBtn;
 
         public ViewHolder(View view) {
             super(view);
-            LinearLayout container2 = new LinearLayout(context);
-            LinearLayout.LayoutParams layoutParamsContainer2 = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-            container2.setOrientation(HORIZONTAL);
-            container2.setPadding((int) G.context.getResources().getDimension(messageContainerPadding), 0, 5, 2);
-            container2.setLayoutParams(layoutParamsContainer2);
 
-            image = new AppCompatImageView(G.context);
-            LinearLayout.LayoutParams layoutParamsImage = new LinearLayout.LayoutParams(ViewMaker.i_Dp(R.dimen.dp48), ViewMaker.i_Dp(R.dimen.dp48));
-            layoutParamsImage.rightMargin = 14;
-            image.setId(R.id.image);
-            image.setContentDescription(null);
-            AppUtils.setImageDrawable(image, R.mipmap.user);
-            image.setLayoutParams(layoutParamsImage);
+            /**
+             * contact image
+             * */
 
-            LinearLayout container3 = new LinearLayout(context);
-            LinearLayout.LayoutParams layoutParamsContainer3 = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-            container3.setOrientation(VERTICAL);
-            container3.setLayoutParams(layoutParamsContainer3);
+            contactImage = new AppCompatImageView(G.context);
+            contactImage.setId(R.id.iv_contactItem_contact);
+            contactImage.setContentDescription(null);
+            AppUtils.setImageDrawable(contactImage, R.mipmap.user);
 
-            name = new AppCompatTextView(G.context);
-            LinearLayout.LayoutParams layoutParamsName = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
-            name.setId(R.id.name);
-            name.setTextAppearance(context, android.R.style.TextAppearance_Medium);
-            name.setTextColor(Color.parseColor(G.textBubble));
-            name.setText("Contact Name");
-            ViewMaker.setTextSize(name, R.dimen.dp14);
-            ViewMaker.setTypeFace(name);
-            name.setLayoutParams(layoutParamsName);
-            container3.addView(name);
+            /**
+             * contact name
+             * */
 
-            number = new AppCompatTextView(G.context);
-            LinearLayout.LayoutParams layoutParamsNumber = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
-            number.setId(R.id.number);
-            number.setTextAppearance(context, android.R.style.TextAppearance_Small);
-            ViewMaker.setTypeFace(number);
+            contactName = new AppCompatTextView(G.context);
+            contactName.setId(R.id.tv_contactItem_contactName);
+            contactName.setTextColor(Color.parseColor(G.textBubble));
+            ViewMaker.setTextSize(contactName, R.dimen.dp14);
+            ViewMaker.setTypeFace(contactName);
 
-            number.setTextColor(Color.parseColor(G.textBubble));
-            number.setText("Contact Number");
-            number.setLayoutParams(layoutParamsNumber);
 
-            container3.addView(number);
-            container2.addView(image);
-            container2.addView(container3);
+            /**
+             * contact number
+             * */
 
-            m_container.addView(container2);
+            contactNumber = new AppCompatTextView(G.context);
+            contactNumber.setId(R.id.tv_contactItem_contactNumber);
+            ViewMaker.setTypeFace(contactNumber);
+            contactNumber.setTextColor(Color.parseColor(G.textBubble));
+
+            /**
+             * view contact button
+             * */
+
+            viewContactBtn = new Button(getContext());
+            viewContactBtn.setId(R.id.btn_contactItem_viewContact);
+            viewContactBtn.setText(getContext().getResources().getString(R.string.view_contact));
+            viewContactBtn.setBackground(getDrawable(R.drawable.background_view_contact));
+            viewContactBtn.setPadding(LayoutCreator.dp(16), 0, LayoutCreator.dp(16), 0);
+            viewContactBtn.setTextSize(12);
+            viewContactBtn.setTextColor(getColor(R.color.md_blue_500));
+            viewContactBtn.setAllCaps(false);
+
+
+            /**
+             * root view
+             * */
+
+            rootView = new ConstraintLayout(getContext());
+            set = new ConstraintSet();
+
+
+            /**
+             * set views dependency
+             * */
+
+            set.constrainWidth(contactImage.getId(), LayoutCreator.dp(35));
+            set.constrainHeight(contactImage.getId(), LayoutCreator.dp(35));
+
+            set.constrainWidth(contactNumber.getId(), ConstraintSet.WRAP_CONTENT);
+            set.constrainHeight(contactNumber.getId(), ConstraintSet.WRAP_CONTENT);
+
+            set.constrainWidth(contactName.getId(), ConstraintSet.WRAP_CONTENT);
+            set.constrainHeight(contactName.getId(), ConstraintSet.WRAP_CONTENT);
+
+
+            set.connect(contactImage.getId(), ConstraintSet.TOP, ConstraintSet.PARENT_ID, ConstraintSet.TOP, LayoutCreator.dp(8));
+            set.connect(contactImage.getId(), ConstraintSet.BOTTOM, ConstraintSet.PARENT_ID, ConstraintSet.BOTTOM, LayoutCreator.dp(8));
+            set.connect(contactImage.getId(), ConstraintSet.LEFT, ConstraintSet.PARENT_ID, ConstraintSet.LEFT, LayoutCreator.dp(8));
+            rootView.addView(contactImage);
+
+
+            set.connect(contactName.getId(), ConstraintSet.LEFT, contactImage.getId(), ConstraintSet.RIGHT, LayoutCreator.dp(8));
+            rootView.addView(contactName);
+
+            set.connect(contactNumber.getId(), ConstraintSet.LEFT, contactName.getId(), ConstraintSet.LEFT);
+            rootView.addView(contactNumber);
+
+            int[] chainViews = {contactName.getId(), contactNumber.getId()};
+            float[] chainWeights = {0, 0};
+            set.createVerticalChain(contactImage.getId(), ConstraintSet.TOP, contactImage.getId(), ConstraintSet.BOTTOM,
+                    chainViews, chainWeights, ConstraintSet.CHAIN_PACKED);
+
+
+            set.applyTo(rootView);
+            getContentBloke().addView(rootView, 0);
+            getContentBloke().addView(viewContactBtn, 1, LayoutCreator.createFrame(200, 30, Gravity.CENTER,
+                    4, 4, 4, 0));
         }
     }
 }
