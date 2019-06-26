8dd56613a4df8314fc656d1bf4b4050627b30a76
a.nazari <a.nazari@kianiranian.com>
2019-06-26 18:08:28 +0430

    fix all multi select and call ,shared media button ui

--- app/src/main/java/net/iGap/fragments/FragmentMain.java
+++ app/src/main/java/net/iGap/fragments/FragmentMain.java
@@ -280,13 +280,13 @@ public class FragmentMain extends BaseFragment implements ToolbarListener, Activ
     }
 
     private void setColorToDarkMode(TextView textView) {
-        textView.setBackground(getContext().getResources().getDrawable(R.drawable.background_multi_select_dark));
-        textView.setTextColor(getContext().getResources().getColor(R.color.gray10));
+        textView.setBackground(getContext().getResources().getDrawable(R.drawable.round_button_enabled_bg));
+        textView.setTextColor(getContext().getResources().getColor(R.color.white));
     }
 
     private void setColorToLightMode(TextView textView) {
-        textView.setBackground(getContext().getResources().getDrawable(R.drawable.background_multi_select_light));
-        textView.setTextColor(getContext().getResources().getColor(R.color.black));
+        textView.setBackground(getContext().getResources().getDrawable(R.drawable.round_button_disabled_bg));
+        textView.setTextColor(getContext().getResources().getColor(R.color.gray_4c));
     }
 
     private void refreshChatList(int pos, boolean isRefreshAll) {
