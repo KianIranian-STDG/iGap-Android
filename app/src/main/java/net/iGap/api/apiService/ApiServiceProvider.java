d3911933a5ca1f8c9b9ed2f96923e2a43f94a73f
aabolfazl <a.abbasi@kianiranian.com>
2019-07-07 11:40:22 +0430

    Add channel api service

--- app/src/main/java/net/iGap/api/apiService/ApiServiceProvider.java
+++ app/src/main/java/net/iGap/api/apiService/ApiServiceProvider.java
@@ -1,16 +1,26 @@
 package net.iGap.api.apiService;
 
 import net.iGap.api.BeepTunesApi;
+import net.iGap.api.ChannelApi;
 
 public class ApiServiceProvider {
-    private static BeepTunesApi beepTunesApi;
     private static RetrofitFactory factory = new RetrofitFactory();
+
+    private static BeepTunesApi beepTunesApi;
+    private static ChannelApi channelApi;
 
     public static BeepTunesApi getBeepTunesClient() {
         if (beepTunesApi == null) {
             beepTunesApi = factory.getRetrofit(ApiStatic.BEEP_TUNES).create(BeepTunesApi.class);
         }
         return beepTunesApi;
+    }
+
+    public static ChannelApi getChannelApi() {
+        if (channelApi == null) {
+            channelApi = factory.getRetrofit(ApiStatic.CHANNEL).create(ChannelApi.class);
+        }
+        return channelApi;
     }
 
 }
