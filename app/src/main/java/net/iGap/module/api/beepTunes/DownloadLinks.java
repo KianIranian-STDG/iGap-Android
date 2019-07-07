1f23536d4d5c619ab7be51e40a901ef1821160f4
aabolfazl <a.abbasi@kianiranian.com>
2019-07-07 17:05:06 +0430

    fix bug

--- app/src/main/java/net/iGap/module/api/beepTunes/DownloadLinks.java
+++ app/src/main/java/net/iGap/module/api/beepTunes/DownloadLinks.java
@@ -3,10 +3,6 @@ package net.iGap.module.api.beepTunes;
 
 import com.google.gson.annotations.SerializedName;
 
-import javax.annotation.Generated;
-
-@Generated("net.hexar.json2pojo")
-@SuppressWarnings("unused")
 public class DownloadLinks {
 
     @SerializedName("H360")
--- app/src/main/java/net/iGap/module/api/beepTunes/Genre.java
+++ app/src/main/java/net/iGap/module/api/beepTunes/Genre.java
@@ -3,10 +3,6 @@ package net.iGap.module.api.beepTunes;
 
 import com.google.gson.annotations.Expose;
 
-import javax.annotation.Generated;
-
-@Generated("net.hexar.json2pojo")
-@SuppressWarnings("unused")
 public class Genre {
 
     @Expose
