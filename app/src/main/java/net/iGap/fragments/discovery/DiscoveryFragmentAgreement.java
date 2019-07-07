ceab882b3f8cd05eef0a700bf52156b5fa6e5166
Mehdi.Bagvand <m.bagvand@kianiranian.com>
2019-07-07 15:32:32 +0430

    fix bug in agreement discovery

--- app/src/main/java/net/iGap/fragments/discovery/DiscoveryFragmentAgreement.java
+++ app/src/main/java/net/iGap/fragments/discovery/DiscoveryFragmentAgreement.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.widget.SwipeRefreshLayout;
+import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.CheckBox;
@@ -138,7 +139,7 @@ public class DiscoveryFragmentAgreement extends FragmentToolBarBack {
             scroll_view.setVisibility(View.VISIBLE);
             emptyRecycle.setVisibility(View.GONE);
             agreement.setVisibility(View.VISIBLE);
-            agreement.setText(body);
+            agreement.setText(Html.fromHtml(body).toString());
             pullToRefresh.setRefreshing(false);
         });
     }
