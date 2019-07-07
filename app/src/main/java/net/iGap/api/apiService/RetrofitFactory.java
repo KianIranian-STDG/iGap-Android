87afe1d5e380d6d1d100afddb9f0e40dd7e49a0b
aabolfazl <a.abbasi@kianiranian.com>
2019-07-07 11:26:49 +0430

    Add api service factory for all rest api service

---
+++ app/src/main/java/net/iGap/api/apiService/RetrofitFactory.java
@@ -0,0 +1,38 @@
+package net.iGap.api.apiService;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class RetrofitFactory {
+    private OkHttpClient httpClient;
+
+    RetrofitFactory() {
+        OkHttpClient.Builder builder = new OkHttpClient.Builder();
+        builder.addInterceptor(chain -> {
+            Request original = chain.request();
+            Request request = original.newBuilder()
+                    .header("Authorization", ApiStatic.USER_TOKEN)
+                    .header("Content-Type", "application/json")
+                    .method(original.method(), original.body())
+                    .build();
+            return chain.proceed(request);
+        });
+        httpClient = builder.build();
+    }
+
+    Retrofit getRetrofit(String clientType) {
+        if (clientType.equals(ApiStatic.BEEP_TUNES)) {
+
+            Retrofit retrofit = new Retrofit.Builder()
+                    .baseUrl(ApiStatic.BEEP_TUNES_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .client(httpClient)
+                    .build();
+            return retrofit;
+
+        } else
+            return null;
+    }
+}
