786ab8c85879b61c316b872ed79b0e1f75f14f7e
Mehdi.Bagvand <m.bagvand@kianiranian.com>
2019-07-08 15:23:09 +0430

    fix bug relation between realm and ContactList (Contact delete!)

--- app/src/main/java/net/iGap/helper/ContactManager.java
+++ app/src/main/java/net/iGap/helper/ContactManager.java
@@ -3,9 +3,10 @@ package net.iGap.helper;
 import net.iGap.realm.RealmContacts;
 import net.iGap.realm.RealmContactsFields;
 
+import java.util.List;
+
 import io.realm.Realm;
 import io.realm.RealmList;
-import io.realm.RealmResults;
 
 public class ContactManager {
     public static final int LOAD_AVG = 30;
@@ -13,7 +14,7 @@ public class ContactManager {
 
     public static final String FIRST = "FIRST";
     public static final String OVER_LOAD = "OVERLOAD";
-    private static RealmResults<RealmContacts> results;
+    private static List<RealmContacts> results;
 
     private static int first = 0;
     private static int loadMore = LOAD_AVG;
@@ -51,25 +52,11 @@ public class ContactManager {
     }
 
     private static void getIgapContact() {
-
-
         if (results == null) {
             Realm realm = Realm.getDefaultInstance();
-//            realm.executeTransaction(realm1 -> {
-//                RealmContacts contacts = realm1.where(RealmContacts.class).sort(RealmContactsFields.DISPLAY_NAME).findFirst();
-//                for (int i = 0; i < 5000; i++) {
-//                    RealmContacts object = realm1.createObject(RealmContacts.class);
-//                    object.setDisplay_name(contacts.getDisplay_name());
-//                    object.setId(contacts.getId());
-//                    object.setPhone(contacts.getPhone());
-//                    object.setAvatar(contacts.getAvatar());
-//                    Log.i(TAG, "getIgapContact: " + contacts.getDisplay_name() + " " + i);
-//                }
-//            });
-            realm.executeTransaction(realm1 -> {
-                results = realm1.where(RealmContacts.class).limit(CONTACT_LIMIT).sort(RealmContactsFields.DISPLAY_NAME).findAll();
-                contactSize = results.size();
-            });
+            results = realm.copyFromRealm(realm.where(RealmContacts.class).limit(CONTACT_LIMIT).sort(RealmContactsFields.DISPLAY_NAME).findAll());
+            contactSize = results.size();
+            realm.close();
         }
     }
 
