9fb3a2504df08479397d5276d27c8aa5418139a5
nazanin <n.omrani@kianiranian.com>
2019-07-10 06:00:23 -0400

    working on task

---
+++ app/src/main/java/net/iGap/model/PopularChannel/Advertisement.java
@@ -0,0 +1,62 @@
+
+package net.iGap.model.PopularChannel;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+
+public class Advertisement {
+
+    @SerializedName("id")
+    private String mId;
+    @SerializedName("looped")
+    private Boolean mLooped;
+    @SerializedName("playback_time")
+    private Long mPlaybackTime;
+    @SerializedName("slides")
+    private List<Slide> mSlides;
+    @SerializedName("title")
+    private String mTitle;
+
+    public String getId() {
+        return mId;
+    }
+
+    public void setId(String id) {
+        mId = id;
+    }
+
+    public Boolean getLooped() {
+        return mLooped;
+    }
+
+    public void setLooped(Boolean looped) {
+        mLooped = looped;
+    }
+
+    public Long getPlaybackTime() {
+        return mPlaybackTime;
+    }
+
+    public void setPlaybackTime(Long playbackTime) {
+        mPlaybackTime = playbackTime;
+    }
+
+    public List<Slide> getSlides() {
+        return mSlides;
+    }
+
+    public void setSlides(List<Slide> slides) {
+        mSlides = slides;
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public void setTitle(String title) {
+        mTitle = title;
+    }
+
+}
