29aa4eb19976e2e8f28ebbeb265463e6efe3571e
a.zarghalami <a.zarghalami@kian.local>
2019-07-10 17:39:58 +0430

    change empty view Image to new Image

--- app/src/main/java/net/iGap/fragments/FragmentMain.java
+++ app/src/main/java/net/iGap/fragments/FragmentMain.java
@@ -6,7 +6,6 @@ import android.graphics.Color;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
-import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.constraint.ConstraintLayout;
