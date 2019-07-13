d9df0775f82b45fba6833d64da39949a64868e02
a.zarghalami <a.zarghalami@kian.local>
2019-07-13 14:55:27 +0430

    fixed bug save state in call fragment

--- app/src/main/java/net/iGap/fragments/FragmentCall.java
+++ app/src/main/java/net/iGap/fragments/FragmentCall.java
@@ -309,6 +309,8 @@ public class FragmentCall extends BaseFragment implements OnCallLogClear, Toolba
             }
 
         });
+        //Todo: fixed it, cause load view with delay
+        setViewState(mIsMultiSelectEnable);
     }
 
     private void checkListIsEmpty() {
