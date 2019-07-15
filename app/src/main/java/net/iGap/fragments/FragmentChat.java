cf090c35b0afe7a2bd3741987435e0964650bb5c
Mehdi.Bagvand <m.bagvand@kianiranian.com>
2019-07-15 18:52:33 +0430

    fix another crash!

--- app/src/main/java/net/iGap/fragments/FragmentChat.java
+++ app/src/main/java/net/iGap/fragments/FragmentChat.java
@@ -1238,7 +1238,7 @@ public class FragmentChat extends BaseFragment
                 latestUri = null; // check
             } else {
                 if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
-                    if (data.getClipData() != null) { // multi select file
+                    if (data != null && data.getClipData() != null) { // multi select file
                         listPathString = AttachFile.getClipData(data.getClipData());
 
                         if (listPathString != null) {
