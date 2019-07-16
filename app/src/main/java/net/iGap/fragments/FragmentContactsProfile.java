1f43c85c7b563516730994d195a72b6891e537ef
a.nazari <a.nazari@kianiranian.com>
2019-07-16 09:06:34 +0430

    set dark mode to contact profile views

--- app/src/main/java/net/iGap/fragments/FragmentContactsProfile.java
+++ app/src/main/java/net/iGap/fragments/FragmentContactsProfile.java
@@ -123,6 +123,7 @@ public class FragmentContactsProfile extends BaseFragment {
     public void onViewCreated(@NotNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        checkTheme();
         userAvatarImageView = binding.toolbarAvatar ;
         userAvatarImageView.setOnClickListener(v -> viewModel.onImageClick());
 
@@ -538,6 +539,13 @@ public class FragmentContactsProfile extends BaseFragment {
         });
 
         initialToolbar();
+    }
+
+    private void checkTheme() {
+
+        if (G.isDarkTheme){
+            binding.toolbarFabChat.setBackgroundTintList(ColorStateList.valueOf(getContext().getResources().getColor(R.color.navigation_dark_mode_bg)));
+        }
     }
 
     private final float PERCENTAGE_TO_SHOW_TITLE_AT_TOOLBAR  = 0.6f;
