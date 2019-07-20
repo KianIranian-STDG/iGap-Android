c289d9225f012c4532a555c281050bca685cac61
a.nazari <a.nazari@kianiranian.com>
2019-07-20 16:16:50 +0430

    fix scanner click in toolbar

--- app/src/main/java/net/iGap/helper/HelperToolbar.java
+++ app/src/main/java/net/iGap/helper/HelperToolbar.java
@@ -122,6 +122,7 @@ public class HelperToolbar {
     private int mPassCodeIcon;
     private int mScannerIcon;
     private int mAnimationOldPositionItem = 0;
+    private boolean isRoundBackground = true;
 
     private HelperToolbar() {
     }
@@ -188,6 +189,11 @@ public class HelperToolbar {
 
     public HelperToolbar setLogoShown(boolean logoShown) {
         this.isLogoShown = logoShown;
+        return this;
+    }
+
+    public HelperToolbar setRoundBackground(boolean isRound) {
+        this.isRoundBackground = isRound;
         return this;
     }
 
@@ -1004,29 +1010,28 @@ public class HelperToolbar {
 
     private void onScannerClickListener() {
 
-//        if (!G.isWalletRegister) {
-//            new HelperFragment(mFragmentActivity.getSupportFragmentManager() ,FragmentWalletAgrement.newInstance(ActivityMain.userPhoneNumber.substring(2))).load();
-//        } else {
-//            Intent intent = new Intent(mContext, WalletActivity.class);
-//            intent.putExtra("Language", "fa");
-//            intent.putExtra("Mobile", "0" + ActivityMain.userPhoneNumber.substring(2));
-//            intent.putExtra("PrimaryColor", G.appBarColor);
-//            intent.putExtra("DarkPrimaryColor", G.appBarColor);
-//            intent.putExtra("AccentColor", G.appBarColor);
-//            intent.putExtra("IS_DARK_THEME", G.isDarkTheme);
-//            intent.putExtra(WalletActivity.LANGUAGE, G.selectedLanguage);
-//            intent.putExtra(WalletActivity.PROGRESSBAR, G.progressColor);
-//            intent.putExtra(WalletActivity.LINE_BORDER, G.lineBorder);
-//            intent.putExtra(WalletActivity.BACKGROUND, G.backgroundTheme);
-//            intent.putExtra(WalletActivity.BACKGROUND_2, G.backgroundTheme);
-//            intent.putExtra(WalletActivity.TEXT_TITLE, G.textTitleTheme);
-//            intent.putExtra(WalletActivity.TEXT_SUB_TITLE, G.textSubTheme);
-//            intent.putExtra("isScan",true);
-//            G.fragmentActivity.startActivityForResult(intent, WALLET_REQUEST_CODE);
-//        }
+        if (!G.isWalletRegister) {
+            new HelperFragment(mFragmentActivity.getSupportFragmentManager() ,FragmentWalletAgrement.newInstance(ActivityMain.userPhoneNumber.substring(2))).load();
+        } else {
+            Intent intent = new Intent(mContext, WalletActivity.class);
+            intent.putExtra("Language", "fa");
+            intent.putExtra("Mobile", "0" + ActivityMain.userPhoneNumber.substring(2));
+            intent.putExtra("PrimaryColor", G.appBarColor);
+            intent.putExtra("DarkPrimaryColor", G.appBarColor);
+            intent.putExtra("AccentColor", G.appBarColor);
+            intent.putExtra("IS_DARK_THEME", G.isDarkTheme);
+            intent.putExtra(WalletActivity.LANGUAGE, G.selectedLanguage);
+            intent.putExtra(WalletActivity.PROGRESSBAR, G.progressColor);
+            intent.putExtra(WalletActivity.LINE_BORDER, G.lineBorder);
+            intent.putExtra(WalletActivity.BACKGROUND, G.backgroundTheme);
+            intent.putExtra(WalletActivity.BACKGROUND_2, G.backgroundTheme);
+            intent.putExtra(WalletActivity.TEXT_TITLE, G.textTitleTheme);
+            intent.putExtra(WalletActivity.TEXT_SUB_TITLE, G.textSubTheme);
+            intent.putExtra("isScan",true);
+            G.fragmentActivity.startActivityForResult(intent, WALLET_REQUEST_CODE);
+        }
 
 
-        new HelperFragment(mFragmentActivity.getSupportFragmentManager(), new BeepTunesFragment()).setReplace(false).load();
     }
 
     private void initViews(ViewMaker view) {
