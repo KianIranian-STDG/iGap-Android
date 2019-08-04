62da574eea60940591174d5486bf778bc96c650c
abolfazl <a.abbasi@kianiranian.com>
2019-08-04 10:18:51 +0430

    fix request money dialog dark theme add bank name

--- app/src/main/java/net/iGap/fragments/chatMoneyTransfer/ChatMoneyTransferFragment.java
+++ app/src/main/java/net/iGap/fragments/chatMoneyTransfer/ChatMoneyTransferFragment.java
@@ -108,6 +108,7 @@ public class ChatMoneyTransferFragment extends BottomSheetDialogFragment {
         TextView transferToTvCard = rootView.findViewById(R.id.tv_cardToCard_transferTo);
         TextView amountTvCard = view.findViewById(R.id.tv_chat_card_amountText);
         TextView cardNumberTvCard = view.findViewById(R.id.et_chat_card_cardNumberTv);
+        TextView bankNameTv = view.findViewById(R.id.tv_chat_card_bankName);
         amountEtCard = view.findViewById(R.id.et_chat_card_cardamount);
         cardNumberEtCard = view.findViewById(R.id.et_chat_card_cardNumber);
         descEtCard = view.findViewById(R.id.et_chat_card_desc);
@@ -129,7 +130,6 @@ public class ChatMoneyTransferFragment extends BottomSheetDialogFragment {
         Utils.darkModeHandler(walletTransferIv);
 
         creditTv.setTextColor(Utils.darkModeHandler(getContext()));
-
 
         /**
          * because add fragment in other fragment can not use getFragmentManager
@@ -171,6 +171,31 @@ public class ChatMoneyTransferFragment extends BottomSheetDialogFragment {
 
             }
         });
+
+        cardNumberEtCard.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if (s.length() >= 7) {
+                    bankNameTv.setText(bankName(cardNumberEtCard.getText().toString().replace("-", "")));
+                } else {
+                    bankNameTv.setText("");
+                }
+            }
+        });
+
+        amountEtCard.setTextColor(Utils.darkModeHandler(getContext()));
+        cardNumberEtCard.setTextColor(Utils.darkModeHandler(getContext()));
+        descEtCard.setTextColor(Utils.darkModeHandler(getContext()));
 
     }
 
@@ -224,7 +249,6 @@ public class ChatMoneyTransferFragment extends BottomSheetDialogFragment {
 
             }
         });
-
     }
 
     private void transferActionInit() {
@@ -252,7 +276,7 @@ public class ChatMoneyTransferFragment extends BottomSheetDialogFragment {
             Animation fadeIn = new AlphaAnimation(0, 1);
             fadeIn.setInterpolator(new DecelerateInterpolator());
             fadeIn.setDuration(300);
-            transferRootView.setAnimation(fadeIn);
+            transferRootViewCard.setAnimation(fadeIn);
         });
 
         TextView cardToCardTv = rootView.findViewById(R.id.tv_moneyAction_cardToCard);
@@ -287,6 +311,57 @@ public class ChatMoneyTransferFragment extends BottomSheetDialogFragment {
         } else {
             return R.style.BaseBottomSheetDialogLight;
         }
+    }
+
+    private String bankName(String cardNumber) {
+        if (cardNumber.startsWith("603799"))
+            return "بانک ملی ایران";
+        else if (cardNumber.startsWith("589210"))
+            return "بانک سپه";
+        else if (cardNumber.startsWith("627648"))
+            return "بانک توسعه صادرات";
+        else if (cardNumber.startsWith("627961"))
+            return "بانک صنعت و معدن";
+        else if (cardNumber.startsWith("603770"))
+            return "بانک کشاورزی";
+        else if (cardNumber.startsWith("628023"))
+            return "بانک مسکن";
+        else if (cardNumber.startsWith("627760"))
+            return "پست بانک ایران";
+        else if (cardNumber.startsWith("502908"))
+            return "بانک توسعه تعاون";
+        else if (cardNumber.startsWith("627412"))
+            return "بانک اقتصاد نوین";
+        else if (cardNumber.startsWith("622106"))
+            return "بانک پارسیان";
+        else if (cardNumber.startsWith("502229"))
+            return "بانک پاسارگاد";
+        else if (cardNumber.startsWith("627488"))
+            return "بانک کارآفرین";
+        else if (cardNumber.startsWith("621986"))
+            return "بانک سامان";
+        else if (cardNumber.startsWith("639346"))
+            return "بانک سینا";
+        else if (cardNumber.startsWith("639607"))
+            return "بانک سرمایه";
+        else if (cardNumber.startsWith("502806"))
+            return "بانک شهر";
+        else if (cardNumber.startsWith("502938"))
+            return "بانک دی";
+        else if (cardNumber.startsWith("603769"))
+            return "بانک صادرات";
+        else if (cardNumber.startsWith("610433"))
+            return "بانک ملت";
+        else if (cardNumber.startsWith("627353"))
+            return "بانک تجارت";
+        else if (cardNumber.startsWith("585983"))
+            return "بانک تجارت";
+        else if (cardNumber.startsWith("627381"))
+            return "بانک انصار";
+        else if (cardNumber.startsWith("639370"))
+            return "بانک مهر اقتصاد";
+        else
+            return "";
     }
 
     public interface MoneyTransferAction {
