6453d1def5ac988ac4ca9a2f2112a245fbc7e377
a.nazari <a.nazari@kianiranian.com>
2019-08-19 18:44:39 +0430

    add set pay view model , layout and complete theme and multi language

--- app/src/main/java/net/iGap/fragments/seePay/FragmentSeePay.java
+++ app/src/main/java/net/iGap/fragments/seePay/FragmentSeePay.java
@@ -1,27 +1,76 @@
 package net.iGap.fragments.seePay;
 
 
+import android.arch.lifecycle.ViewModelProviders;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import net.iGap.R;
+import net.iGap.databinding.FragmentSeePayBinding;
+import net.iGap.fragments.BaseFragment;
+import net.iGap.helper.HelperToolbar;
+import net.iGap.interfaces.ToolbarListener;
+import net.iGap.viewmodel.FragmentSeePayViewModel;
 
 
-public class FragmentSeePay extends Fragment {
+public class FragmentSeePay extends BaseFragment implements ToolbarListener {
 
+    private FragmentSeePayViewModel viewModel ;
+    private FragmentSeePayBinding binding ;
 
     public FragmentSeePay() {
 
     }
 
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        viewModel = ViewModelProviders.of(this).get(FragmentSeePayViewModel.class);
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-
-        return inflater.inflate(R.layout.fragment_see_pay, container, false);
+        binding = DataBindingUtil.inflate(inflater ,R.layout.fragment_see_pay, container, false);
+        binding.setViewModel(viewModel);
+        binding.setLifecycleOwner(getViewLifecycleOwner());
+        return binding.getRoot();
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        initToolbar();
+
+    }
+
+    private void initToolbar() {
+
+        HelperToolbar toolbar = HelperToolbar.create()
+                .setContext(getContext())
+                .setLogoShown(true)
+                .setDefaultTitle(getString(R.string.see_pay_title))
+                .setLeftIcon(R.string.back_icon)
+                .setRightIcons(R.string.history_icon)
+                .setListener(this);
+
+        binding.fspToolbar.addView(toolbar.getView());
+
+    }
+
+    @Override
+    public void onLeftIconClickListener(View view) {
+        popBackStackFragment();
+    }
+
+    @Override
+    public void onRightIconClickListener(View view) {
+
+    }
 }
---
+++ app/src/main/java/net/iGap/viewmodel/FragmentSeePayViewModel.java
@@ -0,0 +1,21 @@
+package net.iGap.viewmodel;
+
+
+import android.arch.lifecycle.ViewModel;
+
+public class FragmentSeePayViewModel extends ViewModel {
+
+
+    public FragmentSeePayViewModel() {
+    }
+
+    public void onAddCarClick(){
+
+    }
+
+    public void onInquiryClick(){
+
+    }
+
+
+}
