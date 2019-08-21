02f179682cd97e1e58e6e6297d81cd2023c833ac
a.nazari <a.nazari@kianiranian.com>
2019-08-21 18:15:42 +0430

    complete cpay history ui , view model , actions

--- app/src/main/java/net/iGap/fragments/cPay/FragmentCPayHistory.java
+++ app/src/main/java/net/iGap/fragments/cPay/FragmentCPayHistory.java
@@ -1,24 +1,70 @@
 package net.iGap.fragments.cPay;
 
 
+import android.arch.lifecycle.ViewModelProviders;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import net.iGap.R;
+import net.iGap.databinding.FragmentCpayHistoryBinding;
+import net.iGap.fragments.BaseFragment;
+import net.iGap.helper.HelperToolbar;
+import net.iGap.interfaces.ToolbarListener;
+import net.iGap.viewmodel.FragmentCPayHistoryViewModel;
 
-public class FragmentCPayHistory extends Fragment {
+public class FragmentCPayHistory extends BaseFragment implements ToolbarListener {
 
+    private FragmentCPayHistoryViewModel viewModel ;
+    private FragmentCpayHistoryBinding binding ;
 
     public FragmentCPayHistory() {
     }
 
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        viewModel = ViewModelProviders.of(this).get(FragmentCPayHistoryViewModel.class);
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_cpay_history, container, false);
+        binding = DataBindingUtil.inflate(inflater , R.layout.fragment_cpay_history, container, false);
+        binding.setViewModel(viewModel);
+        binding.setLifecycleOwner(getViewLifecycleOwner());
+        return binding.getRoot();
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        initToolbar();
+        initRecyclerView();
+    }
+
+    private void initRecyclerView() {
+    }
+
+    private void initToolbar() {
+
+        HelperToolbar toolbar = HelperToolbar.create()
+                .setContext(getContext())
+                .setLogoShown(true)
+                .setDefaultTitle(getString(R.string.see_pay_title))
+                .setLeftIcon(R.string.back_icon)
+                .setListener(this);
+
+        binding.fchToolbar.addView(toolbar.getView());
+
+    }
+
+    @Override
+    public void onLeftIconClickListener(View view) {
+        popBackStackFragment();
+    }
 }
---
+++ app/src/main/java/net/iGap/viewmodel/FragmentCPayHistoryViewModel.java
@@ -0,0 +1,21 @@
+package net.iGap.viewmodel;
+
+import android.arch.lifecycle.ViewModel;
+
+public class FragmentCPayHistoryViewModel extends ViewModel {
+
+    public FragmentCPayHistoryViewModel() {
+    }
+
+    public void onFilterClicked(){
+
+    }
+
+    public void onTodayClicked(){
+
+    }
+
+    public void onFromFirstClicked(){
+
+    }
+}
