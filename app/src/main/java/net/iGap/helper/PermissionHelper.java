6c6c1b3efd1ad7434c7f949701b03d3fbc462152
a.zarghalami <a.zarghalami@kian.local>
2019-08-24 13:29:38 +0430

    change bill payment fragment and viewModel

---
+++ app/src/main/java/net/iGap/helper/PermissionHelper.java
@@ -0,0 +1,60 @@
+package net.iGap.helper;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.support.v4.app.ActivityCompat;
+
+public class PermissionHelper {
+
+    private Activity activity;
+    public final static int CameraAndVoicePermissionRequestCode = 100;
+    public final static int VoicePermissionRequestCode = 110;
+    public final static int CameraPermissionRequestCode = 120;
+
+    public PermissionHelper(Activity activity) {
+        this.activity = activity;
+    }
+
+    public boolean grantCameraAndVoicePermission() {
+        String[] Permissions = {Manifest.permission.CAMERA, Manifest.permission.RECORD_AUDIO};
+        if (hasPermissions(Permissions)) {
+            return true;
+        } else {
+            ActivityCompat.requestPermissions(activity, Permissions, CameraAndVoicePermissionRequestCode);
+            return false;
+        }
+    }
+
+    public boolean grantVoicePermission() {
+        String[] Permissions = {Manifest.permission.RECORD_AUDIO};
+        if (hasPermissions(Permissions)) {
+            return true;
+        } else {
+            ActivityCompat.requestPermissions(activity, Permissions, VoicePermissionRequestCode);
+            return false;
+        }
+    }
+
+    public boolean grantCameraPermission() {
+        String[] Permissions = {Manifest.permission.CAMERA};
+        if (hasPermissions(Permissions)) {
+            return true;
+        } else {
+            ActivityCompat.requestPermissions(activity, Permissions, CameraPermissionRequestCode);
+            return false;
+        }
+    }
+
+    private boolean hasPermissions(String... permissions) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && permissions != null) {
+            for (String permission : permissions) {
+                if (ActivityCompat.checkSelfPermission(activity, permission) != PackageManager.PERMISSION_GRANTED) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
