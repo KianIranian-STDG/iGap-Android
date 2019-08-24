bc736379a99c20f5271ea358ecd039f943197410
h.amini <h.amini1993@yahoo.com>
2019-08-24 09:12:23 +0430

    Kuknos -> add old code for repair

--- app/src/main/java/net/iGap/fragments/BottomNavigationFragment.java
+++ app/src/main/java/net/iGap/fragments/BottomNavigationFragment.java
@@ -49,8 +49,10 @@ public class BottomNavigationFragment extends Fragment implements OnUnreadChange
         Fragment fragment;
         switch (position) {
             case 0:
+                //fragment = fragmentManager.findFragmentByTag(RegisteredContactsFragment.class.getName());
                 fragment = fragmentManager.findFragmentByTag(KuknosLoginFrag.class.getName());
                 if (fragment == null) {
+                    //fragment = RegisteredContactsFragment.newInstance(false, false, RegisteredContactsFragment.CONTACTS);
                     fragment = KuknosLoginFrag.newInstance();
                     fragmentTransaction.addToBackStack(fragment.getClass().getName());
                 }
