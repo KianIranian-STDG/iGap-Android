be400ea2ebe68921b20582c7c64288ecbe4b29a0
a.nazari <a.nazari@kianiranian.com>
2019-08-25 12:26:06 +0430

    add plaque api and repository

--- app/src/main/java/net/iGap/api/CPayApi.java
+++ app/src/main/java/net/iGap/api/CPayApi.java
@@ -2,9 +2,11 @@ package net.iGap.api;
 
 import net.iGap.model.cPay.RegisterPlaqueBodyModel;
 import net.iGap.model.cPay.RegisterPlaqueModel;
+import net.iGap.model.cPay.UserPlaquesModel;
 
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.GET;
 import retrofit2.http.POST;
 
 public interface CPayApi {
@@ -12,4 +14,6 @@ public interface CPayApi {
     @POST("register")
     Call<RegisterPlaqueModel> getRegisterNewPlaque(@Body RegisterPlaqueBodyModel body);
 
+    @GET("user-plates")
+    Call<UserPlaquesModel> getUserPlaques();
 }
--- app/src/main/java/net/iGap/api/repository/CPayRepository.java
+++ app/src/main/java/net/iGap/api/repository/CPayRepository.java
@@ -1,15 +1,16 @@
 package net.iGap.api.repository;
 
-import android.util.Log;
-
 import net.iGap.api.CPayApi;
 import net.iGap.api.apiService.RetrofitFactory;
 import net.iGap.api.errorhandler.ErrorHandler;
 import net.iGap.api.errorhandler.ResponseCallback;
 import net.iGap.model.cPay.RegisterPlaqueBodyModel;
 import net.iGap.model.cPay.RegisterPlaqueModel;
+import net.iGap.model.cPay.UserPlaquesModel;
 
 import org.jetbrains.annotations.NotNull;
+
+import java.io.IOException;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -17,32 +18,56 @@ import retrofit2.Response;
 
 public class CPayRepository {
 
-    private static CPayRepository instance ;
-    private CPayApi api ;
+    private static CPayRepository instance;
+    private CPayApi api;
 
     private CPayRepository() {
         api = new RetrofitFactory().getCPayApi();
     }
 
-    public static CPayRepository getInstance(){
-        if(instance == null){
+    public static CPayRepository getInstance() {
+        if (instance == null) {
             instance = new CPayRepository();
         }
         return instance;
     }
 
-    public void registerNewPlaque(RegisterPlaqueBodyModel body , ResponseCallback<RegisterPlaqueModel> callback){
+    public void getAllUserPlaques(ResponseCallback<UserPlaquesModel> callback) {
+
+        api.getUserPlaques().enqueue(new Callback<UserPlaquesModel>() {
+            @Override
+            public void onResponse(Call<UserPlaquesModel> call, Response<UserPlaquesModel> response) {
+                if (response.isSuccessful()) {
+                    callback.onSuccess(response.body());
+                } else {
+                    try {
+                        callback.onError(new ErrorHandler().getError(response.code(), response.errorBody().string()));
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+
+            @Override
+            public void onFailure(Call<UserPlaquesModel> call, Throwable t) {
+                callback.onFailed(new ErrorHandler().checkHandShakeFailure(t));
+            }
+        });
+
+    }
+
+    public void registerNewPlaque(RegisterPlaqueBodyModel body, ResponseCallback<RegisterPlaqueModel> callback) {
 
         api.getRegisterNewPlaque(body).enqueue(new Callback<RegisterPlaqueModel>() {
             @Override
             public void onResponse(@NotNull Call<RegisterPlaqueModel> call, @NotNull Response<RegisterPlaqueModel> response) {
-                if (response.isSuccessful()){
+                if (response.isSuccessful()) {
                     callback.onSuccess(response.body());
-                }else {
-                    try{
-                        callback.onError(new ErrorHandler().getError(response.code() , response.errorBody().string()));
-                    }catch (Exception e){
-                        Log.e("errorr" , "ee" , e);
+                } else {
+                    try {
+                        callback.onError(new ErrorHandler().getError(response.code(), response.errorBody().string()));
+                    } catch (IOException e) {
+                        e.printStackTrace();
                     }
                 }
             }
@@ -55,4 +80,6 @@ public class CPayRepository {
         });
 
     }
+
+
 }
