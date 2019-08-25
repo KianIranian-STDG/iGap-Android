2aa387b2da3481bed7319cc7bdbcc30e4d95df0a
a.zarghalami <a.zarghalami@kian.local>
2019-08-25 14:30:28 +0430

    change igasht buy ticket

--- app/src/main/java/net/iGap/igasht/locationdetail/buyticket/AddBottomSheetListener.java
+++
@@ -1,7 +0,0 @@
-package net.iGap.igasht.locationdetail.buyticket;
-
-public interface AddBottomSheetListener<T> {
-    void setTicketCount(T data);
-
-    void onBackPressed();
-}
---
+++ app/src/main/java/net/iGap/igasht/locationdetail/buyticket/TicketListCountChangeListener.java
@@ -0,0 +1,5 @@
+package net.iGap.igasht.locationdetail.buyticket;
+
+public interface TicketListCountChangeListener {
+    void setTicketTotalPrice(int totalPrice);
+}
