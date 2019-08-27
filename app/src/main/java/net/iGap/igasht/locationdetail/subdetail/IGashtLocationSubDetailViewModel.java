a1347967acfe9c493445f1e9c80a7e5b1ee0871e
nazanin <n.omrani@kianiranian.com>
2019-08-27 17:28:32 +0430

    check parking

--- app/src/main/java/net/iGap/igasht/locationdetail/subdetail/IGashtLocationSubDetailViewModel.java
+++ app/src/main/java/net/iGap/igasht/locationdetail/subdetail/IGashtLocationSubDetailViewModel.java
@@ -1,14 +1,17 @@
 package net.iGap.igasht.locationdetail.subdetail;
 
+import android.databinding.ObservableBoolean;
 import android.databinding.ObservableInt;
 import android.view.View;
 
+import net.iGap.R;
 import net.iGap.igasht.BaseIGashtViewModel;
 import net.iGap.igasht.IGashtRepository;
 import net.iGap.igasht.locationlist.ExtraDetail;
 
 public class IGashtLocationSubDetailViewModel extends BaseIGashtViewModel {
-    private ObservableInt showText= new ObservableInt(View.GONE);
+    private ObservableInt showText = new ObservableInt(View.GONE);
+    private ObservableInt haveParking = new ObservableInt(R.string.close);
     private IGashtRepository repository;
 
     public IGashtLocationSubDetailViewModel() {
@@ -20,6 +23,13 @@ public class IGashtLocationSubDetailViewModel extends BaseIGashtViewModel {
             showMainView.set(View.GONE);
             showText.set(View.VISIBLE);
         }
+        if(getLocationDetail()!=null){
+        haveParking.set(getLocationDetail().getParking() ? R.string.check_icon : R.string.close);}
+
+    }
+
+    public ObservableInt getHaveParking() {
+        return haveParking;
     }
 
     public ObservableInt getShowText() {
@@ -34,4 +44,5 @@ public class IGashtLocationSubDetailViewModel extends BaseIGashtViewModel {
     public void onSuccess(Object data) {
 
     }
+
 }
