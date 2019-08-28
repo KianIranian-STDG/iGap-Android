18cc24669adcb0cd5f6e20b09ea10af1bd562cef
nazanin <n.omrani@kianiranian.com>
2019-08-28 10:25:07 +0430

    make observable

--- app/src/main/java/net/iGap/igasht/locationdetail/subdetail/IGashtLocationSubDetailViewModel.java
+++ app/src/main/java/net/iGap/igasht/locationdetail/subdetail/IGashtLocationSubDetailViewModel.java
@@ -1,6 +1,5 @@
 package net.iGap.igasht.locationdetail.subdetail;
 
-import android.databinding.ObservableBoolean;
 import android.databinding.ObservableInt;
 import android.view.View;
 
@@ -10,21 +9,29 @@ import net.iGap.igasht.IGashtRepository;
 import net.iGap.igasht.locationlist.ExtraDetail;
 
 public class IGashtLocationSubDetailViewModel extends BaseIGashtViewModel {
-    private ObservableInt showText = new ObservableInt(View.GONE);
-    private ObservableInt haveParking = new ObservableInt(R.string.close);
+    private ObservableInt nodetail = new ObservableInt(View.GONE);
+    private ObservableInt haveParking = new ObservableInt(R.string.close_icon);
+    private ObservableInt haveWc=new ObservableInt(R.string.close_icon);
+    private ObservableInt haveCoffeeShop=new ObservableInt(R.string.close_icon);
+    private ObservableInt havePrayerRoom=new ObservableInt(R.string.close_icon);
+    private ObservableInt haveResturant=new ObservableInt(R.string.close_icon);
+    private ObservableInt haveWeelchairRamp=new ObservableInt(R.string.close_icon);
+    private ObservableInt haveWiFi=new ObservableInt(R.string.close_icon);
     private IGashtRepository repository;
 
     public IGashtLocationSubDetailViewModel() {
         repository = IGashtRepository.getInstance();
         if (getLocationDetail() != null) {
             showMainView.set(View.VISIBLE);
-            showText.set(View.GONE);
+            nodetail.set(View.GONE);
         } else {
             showMainView.set(View.GONE);
-            showText.set(View.VISIBLE);
+            nodetail.set(View.VISIBLE);
         }
-        if(getLocationDetail()!=null){
-        haveParking.set(getLocationDetail().getParking() ? R.string.check_icon : R.string.close);}
+        if (getLocationDetail() != null) {
+            haveParking.set(getLocationDetail().getParking() ? R.string.check_icon : R.string.close_icon);
+            haveWc.set(getLocationDetail().getWc()?R.string.check_icon:R.string.close_icon);
+        }
 
     }
 
@@ -33,7 +40,7 @@ public class IGashtLocationSubDetailViewModel extends BaseIGashtViewModel {
     }
 
     public ObservableInt getShowText() {
-        return showText;
+        return nodetail;
     }
 
     public ExtraDetail getLocationDetail() {
