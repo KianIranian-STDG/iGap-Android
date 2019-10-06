98e95d7f6e6b9e34bdbbb661d5ba09fd67e6fabd
h.amini <h.amini1993@yahoo.com>
2019-10-06 14:29:14 +0330

    API -> change Payment repository

--- app/src/main/java/net/iGap/payment/PaymentRepository.java
+++ app/src/main/java/net/iGap/payment/PaymentRepository.java
@@ -3,6 +3,7 @@ package net.iGap.payment;
 import androidx.annotation.NonNull;
 
 import net.iGap.api.PaymentApi;
+import net.iGap.api.apiService.ApiInitializer;
 import net.iGap.api.apiService.RetrofitFactory;
 import net.iGap.api.errorhandler.ErrorHandler;
 import net.iGap.api.apiService.ResponseCallback;
@@ -37,48 +38,14 @@ public class PaymentRepository {
     }
 
     public void checkOrder(String orderToken, ResponseCallback<CheckOrderResponse> callBack) {
-        paymentApi.requestCheckOrder(orderToken).enqueue(new Callback<CheckOrderResponse>() {
-            @Override
-            public void onResponse(@NonNull Call<CheckOrderResponse> call, @NonNull Response<CheckOrderResponse> response) {
-                if (response.code() == 200) {
-                    callBack.onSuccess(response.body());
-                } else {
-                    try {
-                        callBack.onError(new ErrorHandler().getError(response.code(), response.errorBody().string()));
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
 
-            @Override
-            public void onFailure(@NonNull Call<CheckOrderResponse> call, @NonNull Throwable t) {
-                t.printStackTrace();
-                callBack.onFailed(new ErrorHandler().checkHandShakeFailure(t));
-            }
-        });
+        new ApiInitializer<CheckOrderResponse>().initAPI(paymentApi.requestCheckOrder(orderToken), callBack);
+
     }
 
     public void checkOrderStatus(String orderId, ResponseCallback<CheckOrderStatusResponse> callback) {
-        paymentApi.requestCheckOrderStatus(orderId).enqueue(new Callback<CheckOrderStatusResponse>() {
-            @Override
-            public void onResponse(@NotNull Call<CheckOrderStatusResponse> call, @NotNull Response<CheckOrderStatusResponse> response) {
-                if (response.code() == 200) {
-                    callback.onSuccess(response.body());
-                } else {
-                    try {
-                        callback.onError(new ErrorHandler().getError(response.code(), response.errorBody().string()));
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
 
-            @Override
-            public void onFailure(@NotNull Call<CheckOrderStatusResponse> call, @NotNull Throwable t) {
-                t.printStackTrace();
-                callback.onFailed(new ErrorHandler().checkHandShakeFailure(t));
-            }
-        });
+        new ApiInitializer<CheckOrderStatusResponse>().initAPI(paymentApi.requestCheckOrderStatus(orderId), callback);
+
     }
 }
