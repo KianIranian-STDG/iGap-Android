50b43717d0c1b527e6dd1534aebe737ba1dac59a
h.amini <h.amini1993@yahoo.com>
2019-10-12 16:17:07 +0330

    News -> change api address.

--- app/src/main/java/net/iGap/api/apiService/ApiStatic.java
+++ app/src/main/java/net/iGap/api/apiService/ApiStatic.java
@@ -2,7 +2,7 @@ package net.iGap.api.apiService;
 
 public class ApiStatic {
     private static final String BASE_URL = "https://api.igap.net/services/v1.0/";
-    private static final String NEWS_BASE_URL = "http://178.173.144.127:9090/";
+    private static final String NEWS_BASE_URL = "https://api.cafetitle.com/";
     static final String BEEP_TUNES_URL = BASE_URL + "beep-tunes/";
     static final String CHANNEL_URL = BASE_URL + "channel/";
     static final String KUKNOS_URL = BASE_URL + "kuknos/";
@@ -12,6 +12,6 @@ public class ApiStatic {
     static final String CPAY_URL = BASE_URL + "ati/c-pay/";
     static final String CHARITY_URL = BASE_URL + "charity/";
     static final String KUKNOS_Horizan_Server = "https://hz1-test.kuknos.org/";
-    static final String NEWS_URL = NEWS_BASE_URL + "chapar/api/";
+    static final String NEWS_URL = NEWS_BASE_URL + "";
 }
 
