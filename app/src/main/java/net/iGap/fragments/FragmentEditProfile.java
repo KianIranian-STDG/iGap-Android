e6421dcf0e434f6ccd5e63d6471226f1dbd38d86
nazanin omrani <nazanin.omrani92@gmail.com>
2019-10-15 15:11:03 -0700

    working on Fragment

--- app/src/main/java/net/iGap/fragments/FragmentEditProfile.java
+++ app/src/main/java/net/iGap/fragments/FragmentEditProfile.java
@@ -1,5 +1,6 @@
 package net.iGap.fragments;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -7,10 +8,19 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.lifecycle.ViewModelProviders;
 
 import net.iGap.R;
+import net.iGap.databinding.FragmentEditProfileBinding;
+import net.iGap.module.SHP_SETTING;
+import net.iGap.viewmodel.UserProfileViewModel;
 
 public class FragmentEditProfile extends BaseFragment {
+    private UserProfileViewModel viewModel;
+    private FragmentEditProfileBinding binding;
+
     public static FragmentEditProfile newInstance() {
 
         Bundle args = new Bundle();
@@ -20,11 +30,22 @@ public class FragmentEditProfile extends BaseFragment {
         return fragment;
     }
 
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        viewModel = ViewModelProviders.of(this, new ViewModelProvider.Factory() {
+            @NonNull
+            @Override
+            public <T extends ViewModel> T create(@NonNull Class<T> modelClass) {
+                return (T) new UserProfileViewModel(getContext().getSharedPreferences(SHP_SETTING.FILE_NAME, Context.MODE_PRIVATE), avatarHandler);
+            }
+        }).get(UserProfileViewModel.class);
+    }
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_edit_profile, container, false);
         return view;
     }
-
 }
