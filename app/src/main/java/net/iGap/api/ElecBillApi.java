04af0d41a11fcf8a111055fd2a7e2c282d7ac3f2
h.amini <h.amini1993@yahoo.com>
2019-10-21 17:09:58 +0330

    elec_bill -> add model class + api

--- app/src/main/java/net/iGap/api/ElecBillApi.java
+++ app/src/main/java/net/iGap/api/ElecBillApi.java
@@ -1,6 +1,16 @@
 package net.iGap.api;
 
 import net.iGap.electricity_bill.repository.model.Bill;
+import net.iGap.electricity_bill.repository.model.BillData;
+import net.iGap.electricity_bill.repository.model.BranchData;
+import net.iGap.electricity_bill.repository.model.BranchDebit;
+import net.iGap.electricity_bill.repository.model.CompanyList;
+import net.iGap.electricity_bill.repository.model.ElectricityResponse;
+import net.iGap.electricity_bill.repository.model.ElectricityResponseList;
+import net.iGap.electricity_bill.repository.model.ElectricityResponseModel;
+import net.iGap.electricity_bill.repository.model.LastBillData;
+import net.iGap.electricity_bill.repository.model.PaidBill;
+import net.iGap.electricity_bill.repository.model.SaleBill;
 import net.iGap.news.repository.model.NewsGroup;
 import net.iGap.news.repository.model.NewsList;
 
@@ -14,53 +24,50 @@ import retrofit2.http.Query;
 public interface ElecBillApi {
 
     @GET("get-companies")
-    Call<Bill> getCompanies();
+    Call<CompanyList.Company> getCompanies();
 
     @POST("add-bill")
-    Call<Bill> addBill(@Field("bill_identifier") String billID, @Field("mobile_number") String mobileNum,
-                       @Field("national_code") String userNID, @Field("email") String userEmail,
-                       @Field("bill_title") String billTitle, @Field("via_sms") boolean smsEnable,
-                       @Field("via_print") boolean printEnable, @Field("via_email") boolean emailEnable,
-                       @Field("via_app") boolean appEnable);
+    Call<ElectricityResponseModel<String>> addBill(@Field("bill_identifier") String billID, @Field("mobile_number") String mobileNum,
+                                           @Field("national_code") String userNID, @Field("email") String userEmail,
+                                           @Field("bill_title") String billTitle, @Field("via_sms") boolean smsEnable,
+                                           @Field("via_print") boolean printEnable, @Field("via_email") boolean emailEnable,
+                                           @Field("via_app") boolean appEnable);
 
     @POST("edit-bill")
-    Call<Bill> editBill(@Field("bill_identifier") String billID, @Field("mobile_number") String mobileNum,
+    Call<ElectricityResponseModel<String>> editBill(@Field("bill_identifier") String billID, @Field("mobile_number") String mobileNum,
                        @Field("national_code") String userNID, @Field("email") String userEmail,
                        @Field("bill_title") String billTitle, @Field("via_sms") boolean smsEnable,
                        @Field("via_print") boolean printEnable, @Field("via_email") boolean emailEnable,
                        @Field("via_app") boolean appEnable);
 
     @POST("delete-bill")
-    Call<Bill> deleteBill(@Field("bill_identifier") String billID, @Field("mobile_number") String mobileNum,
+    Call<ElectricityResponseModel<String>> deleteBill(@Field("bill_identifier") String billID, @Field("mobile_number") String mobileNum,
                         @Field("national_code") String userNID);
 
     @POST("get-bills")
-    Call<Bill> getBillList(@Field("mobile_number") String mobileNum);
+    Call<ElectricityResponseModel<BillData>> getBillList(@Field("mobile_number") String mobileNum);
 
     @POST("delete-account")
-    Call<Bill> deleteAccount(@Field("mobile_number") String mobileNum, @Field("national_code") String userNID);
+    Call<ElectricityResponseModel<String>> deleteAccount(@Field("mobile_number") String mobileNum, @Field("national_code") String userNID);
 
     @POST("get-branch-info")
-    Call<Bill> getBranchInfo(@Field("bill_identifier") String billID, @Field("mobile_number") String mobileNum);
+    Call<ElectricityResponseModel<BranchData>> getBranchInfo(@Field("bill_identifier") String billID, @Field("mobile_number") String mobileNum);
 
     @POST("get-branch-info")
-    Call<Bill> getBranchDebit(@Field("bill_identifier") String billID, @Field("mobile_number") String mobileNum);
+    Call<ElectricityResponseModel<BranchDebit>> getBranchDebit(@Field("bill_identifier") String billID, @Field("mobile_number") String mobileNum);
 
     @POST("search-bill")
-    Call<Bill> searchBills(@Field("serial_number") String serialNum, @Field("company_code") String companyCode);
+    Call<ElectricityResponseList<BranchData>> searchBills(@Field("serial_number") String serialNum, @Field("company_code") String companyCode);
 
     @POST("get-sale-bills")
-    Call<Bill> getSaleBills(@Field("bill_identifier") String billID, @Field("mobile_number") String mobileNum,
-                          @Field("from_year") String fromYear);
+    Call<ElectricityResponseList<SaleBill>> getSaleBills(@Field("bill_identifier") String billID, @Field("mobile_number") String mobileNum,
+                                                         @Field("from_year") String fromYear);
 
     @POST("get-paid-bills")
-    Call<Bill> getPaiedBills(@Field("bill_identifier") String billID, @Field("mobile_number") String mobileNum,
-                            @Field("from_year") String fromYear);
+    Call<ElectricityResponseList<PaidBill>> getPaiedBills(@Field("bill_identifier") String billID, @Field("mobile_number") String mobileNum,
+                                                          @Field("from_year") String fromYear);
 
     @POST("get-last-bill")
-    Call<Bill> getLastBill(@Field("bill_identifier") String billID, @Field("mobile_number") String mobileNum);
+    Call<ElectricityResponseModel<LastBillData>> getLastBill(@Field("bill_identifier") String billID, @Field("mobile_number") String mobileNum);
 
-    @POST("pay-bill")
-    Call<Bill> getLastBill(@Field("bill_identifier") String billID, @Field("payment_identifier") String payID,
-                           @Field("amount") String amount);
 }
