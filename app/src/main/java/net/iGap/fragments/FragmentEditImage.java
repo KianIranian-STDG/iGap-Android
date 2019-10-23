df81d12dba3d316368ca36913591aa1d00ddad5a
a.nazari <a.nazari@kianiranian.com>
2019-10-23 10:14:37 +0330

    Fix sharing image with text

--- app/src/main/java/net/iGap/fragments/FragmentEditImage.java
+++ app/src/main/java/net/iGap/fragments/FragmentEditImage.java
@@ -707,6 +707,26 @@ public class FragmentEditImage extends BaseFragment {
         return itemGalleryList;
     }
 
+    public static ArrayList<StructBottomSheet> insertItemList(String path, String message,  boolean isSelected) {
+
+        if (itemGalleryList == null) {
+            itemGalleryList = new ArrayList<>();
+        }
+
+        if (!HelperPermission.grantedUseStorage()) {
+            return itemGalleryList;
+        }
+        StructBottomSheet item = new StructBottomSheet();
+        item.setId(itemGalleryList.size());
+        item.setPath(path);
+        item.setText(message);
+        item.isSelected = isSelected;
+        itemGalleryList.add(0, item);
+        textImageList.put(path, item);
+
+        return itemGalleryList;
+    }
+
     public static ArrayList<StructBottomSheet> insertItemList(String path, boolean isSelected, CompleteEditImage completeEdit) {
 
         if (itemGalleryList == null) {
