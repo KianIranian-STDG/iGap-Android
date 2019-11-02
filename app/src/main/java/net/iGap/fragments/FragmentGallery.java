cbc9087c9093753716d4e8de49ba1b539e6e93e6
a.nazari <a.nazari@kianiranian.com>
2019-11-02 10:12:07 +0330

    Add Image Gallery fragment and design ui

---
+++ app/src/main/java/net/iGap/fragments/FragmentGallery.java
@@ -0,0 +1,30 @@
+package net.iGap.fragments;
+
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import net.iGap.R;
+
+public class FragmentGallery extends BaseFragment {
+
+
+    public FragmentGallery() {
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_gallery, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
