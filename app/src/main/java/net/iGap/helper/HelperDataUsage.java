aca46cbcebfd99da6201619e90d3684716a6d280
Mehdi.Bagvand <m.bagvand@kianiranian.com>
2019-11-05 16:17:50 +0330

    inserting DataUsage is a Low priority transaction so make it low.

--- app/src/main/java/net/iGap/helper/HelperDataUsage.java
+++ app/src/main/java/net/iGap/helper/HelperDataUsage.java
@@ -5,127 +5,94 @@ import net.iGap.proto.ProtoGlobal;
 import net.iGap.realm.RealmDataUsage;
 import net.iGap.realm.RealmDataUsageFields;
 
-import io.realm.Realm;
 import io.realm.RealmResults;
 
 public class HelperDataUsage {
 
     public static void increaseDownloadFiles(ProtoGlobal.RoomMessageType type) {
-        new Thread(() -> {
-            ProtoGlobal.RoomMessageType finalType = getDataUsageType(type);
-            DbManager.getInstance().doRealmTask(realm -> {
-                realm.executeTransaction(realm1 -> {
-                    RealmDataUsage realmDataUsage = realm1.where(RealmDataUsage.class)
-                            .equalTo(RealmDataUsageFields.CONNECTIVITY_TYPE, HelperCheckInternetConnection.isConnectivityWifi())
-                            .equalTo(RealmDataUsageFields.TYPE, finalType.toString())
-                            .findFirst();
-                    if (realmDataUsage != null) {
-                        realmDataUsage.setNumDownloadedFile(realmDataUsage.getNumDownloadedFile() + 1);
-                    }
-                });
-            });
-        }).start();
-
+        ProtoGlobal.RoomMessageType finalType = getDataUsageType(type);
+        DbManager.getInstance().doRealmTransactionLowPriorityAsync(realm -> {
+            RealmDataUsage realmDataUsage = realm.where(RealmDataUsage.class)
+                    .equalTo(RealmDataUsageFields.CONNECTIVITY_TYPE, HelperCheckInternetConnection.isConnectivityWifi())
+                    .equalTo(RealmDataUsageFields.TYPE, finalType.toString())
+                    .findFirst();
+            if (realmDataUsage != null) {
+                realmDataUsage.setNumDownloadedFile(realmDataUsage.getNumDownloadedFile() + 1);
+            }
+        });
     }
 
     public static void increaseUploadFiles(ProtoGlobal.RoomMessageType type) {
-        new Thread(() -> {
-            ProtoGlobal.RoomMessageType finalType = getDataUsageType(type);
-            DbManager.getInstance().doRealmTask(realm -> {
-                realm.executeTransaction(realm1 -> {
-                    RealmDataUsage realmDataUsage = realm1.where(RealmDataUsage.class)
-                            .equalTo(RealmDataUsageFields.CONNECTIVITY_TYPE, HelperCheckInternetConnection.isConnectivityWifi())
-                            .equalTo(RealmDataUsageFields.TYPE, finalType.toString())
-                            .findFirst();
-                    if (realmDataUsage != null) {
-                        realmDataUsage.setNumUploadedFiles(realmDataUsage.getNumUploadedFiles() + 1);
-                    }
-                });
-            });
-        }).start();
-
+        ProtoGlobal.RoomMessageType finalType = getDataUsageType(type);
+        DbManager.getInstance().doRealmTransactionLowPriorityAsync(realm -> {
+            RealmDataUsage realmDataUsage = realm.where(RealmDataUsage.class)
+                    .equalTo(RealmDataUsageFields.CONNECTIVITY_TYPE, HelperCheckInternetConnection.isConnectivityWifi())
+                    .equalTo(RealmDataUsageFields.TYPE, finalType.toString())
+                    .findFirst();
+            if (realmDataUsage != null) {
+                realmDataUsage.setNumUploadedFiles(realmDataUsage.getNumUploadedFiles() + 1);
+            }
+        });
     }
 
     public static void progressDownload(long downloadByte, ProtoGlobal.RoomMessageType type) {
-        new Thread(() -> {
-            ProtoGlobal.RoomMessageType finalType = getDataUsageType(type);
-            DbManager.getInstance().doRealmTask(realm -> {
-                realm.executeTransaction(realm1 -> {
-                    RealmDataUsage realmDataUsage = realm1.where(RealmDataUsage.class)
-                            .equalTo(RealmDataUsageFields.CONNECTIVITY_TYPE, HelperCheckInternetConnection.isConnectivityWifi())
-                            .equalTo(RealmDataUsageFields.TYPE, finalType.toString())
-                            .findFirst();
-                    if (realmDataUsage != null) {
-                        realmDataUsage.setDownloadSize(realmDataUsage.getDownloadSize() + downloadByte);
-                    }
-                });
-            });
-        }).start();
-
+        ProtoGlobal.RoomMessageType finalType = getDataUsageType(type);
+        DbManager.getInstance().doRealmTransactionLowPriorityAsync(realm -> {
+            RealmDataUsage realmDataUsage = realm.where(RealmDataUsage.class)
+                    .equalTo(RealmDataUsageFields.CONNECTIVITY_TYPE, HelperCheckInternetConnection.isConnectivityWifi())
+                    .equalTo(RealmDataUsageFields.TYPE, finalType.toString())
+                    .findFirst();
+            if (realmDataUsage != null) {
+                realmDataUsage.setDownloadSize(realmDataUsage.getDownloadSize() + downloadByte);
+            }
+        });
     }
 
 
     public static void progressUpload(long uploadByte, ProtoGlobal.RoomMessageType type) {
-        new Thread(() -> {
-            ProtoGlobal.RoomMessageType finalType = getDataUsageType(type);
-            DbManager.getInstance().doRealmTask(realm -> {
-                realm.executeTransaction(realm1 -> {
-                    RealmDataUsage realmDataUsage = realm1.where(RealmDataUsage.class)
-                            .equalTo(RealmDataUsageFields.CONNECTIVITY_TYPE, HelperCheckInternetConnection.isConnectivityWifi())
-                            .equalTo(RealmDataUsageFields.TYPE, finalType.toString())
-                            .findFirst();
-                    if (realmDataUsage != null) {
-                        realmDataUsage.setUploadSize(realmDataUsage.getUploadSize() + uploadByte);
-                    }
-                });
-            });
-        }).start();
-
+        ProtoGlobal.RoomMessageType finalType = getDataUsageType(type);
+        DbManager.getInstance().doRealmTransactionLowPriorityAsync(realm -> {
+            RealmDataUsage realmDataUsage = realm.where(RealmDataUsage.class)
+                    .equalTo(RealmDataUsageFields.CONNECTIVITY_TYPE, HelperCheckInternetConnection.isConnectivityWifi())
+                    .equalTo(RealmDataUsageFields.TYPE, finalType.toString())
+                    .findFirst();
+            if (realmDataUsage != null) {
+                realmDataUsage.setUploadSize(realmDataUsage.getUploadSize() + uploadByte);
+            }
+        });
     }
 
     public static void initializeRealmDataUsage() {
-        new Thread(() -> {
-            String[] typeList = {"IMAGE", "VIDEO", "AUDIO", "FILE", "UNRECOGNIZED"};
-            DbManager.getInstance().doRealmTask(realm -> {
-                realm.executeTransaction(new Realm.Transaction() {
-                    @Override
-                    public void execute(Realm realm) {
-                        for (String s : typeList) {
-                            RealmDataUsage realmDataUsage = realm.createObject(RealmDataUsage.class);
-                            realmDataUsage.setNumUploadedFiles(0);
-                            realmDataUsage.setNumDownloadedFile(0);
-                            realmDataUsage.setConnectivityType(false);
-                            realmDataUsage.setUploadSize(0);
-                            realmDataUsage.setDownloadSize(0);
-                            realmDataUsage.setType(s);
+        String[] typeList = {"IMAGE", "VIDEO", "AUDIO", "FILE", "UNRECOGNIZED"};
+        DbManager.getInstance().doRealmTransactionLowPriorityAsync(realm -> {
+            for (String s : typeList) {
+                RealmDataUsage realmDataUsage = realm.createObject(RealmDataUsage.class);
+                realmDataUsage.setNumUploadedFiles(0);
+                realmDataUsage.setNumDownloadedFile(0);
+                realmDataUsage.setConnectivityType(false);
+                realmDataUsage.setUploadSize(0);
+                realmDataUsage.setDownloadSize(0);
+                realmDataUsage.setType(s);
 
-                            RealmDataUsage realmDataUsage2 = realm.createObject(RealmDataUsage.class);
-                            realmDataUsage2.setNumUploadedFiles(0);
-                            realmDataUsage2.setNumDownloadedFile(0);
-                            realmDataUsage2.setConnectivityType(true);
-                            realmDataUsage2.setUploadSize(0);
-                            realmDataUsage2.setDownloadSize(0);
-                            realmDataUsage2.setType(s);
-                        }
-                    }
-                });
-            });
-        }).start();
+                RealmDataUsage realmDataUsage2 = realm.createObject(RealmDataUsage.class);
+                realmDataUsage2.setNumUploadedFiles(0);
+                realmDataUsage2.setNumDownloadedFile(0);
+                realmDataUsage2.setConnectivityType(true);
+                realmDataUsage2.setUploadSize(0);
+                realmDataUsage2.setDownloadSize(0);
+                realmDataUsage2.setType(s);
+            }
+        });
     }
 
     public static void clearUsageRealm(boolean connectivityType) {
-        DbManager.getInstance().doRealmTask(realm -> {
+        DbManager.getInstance().doRealmTransaction(realm -> {
             RealmResults<RealmDataUsage> wifiRealmList = realm.where(RealmDataUsage.class).equalTo(RealmDataUsageFields.CONNECTIVITY_TYPE, connectivityType).findAll();
             for (RealmDataUsage usage : wifiRealmList) {
-                realm.executeTransaction(new Realm.Transaction() {
-                    @Override
-                    public void execute(Realm realm) {
-                        usage.setDownloadSize(0);
-                        usage.setUploadSize(0);
-                        usage.setNumDownloadedFile(0);
-                        usage.setNumUploadedFiles(0);
-                    }
-                });
+                usage.setDownloadSize(0);
+                usage.setUploadSize(0);
+                usage.setNumDownloadedFile(0);
+                usage.setNumUploadedFiles(0);
             }
         });
     }
