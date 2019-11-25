760d3952eb7aa1414dac9ca14628acced69f8483
nazanin omrani <nazanin.omrani92@gmail.com>
2019-11-24 16:48:39 -0800

    check player class

--- app/src/main/java/net/iGap/Theme.java
+++ app/src/main/java/net/iGap/Theme.java
@@ -258,6 +258,10 @@ public class Theme {
         return getDrawableAttr(context, R.attr.iGapToolbarBackground);
     }
 
+    public int getUserProfileTabSelector(Context context) {
+        return getDrawableAttr(context, R.attr.iGapProfileStroke);
+    }
+
     public int getToolbarDrawableSharpe(Context context) {
         return getDrawableAttr(context, R.attr.iGapToolbarBackgroundSharp);
     }
