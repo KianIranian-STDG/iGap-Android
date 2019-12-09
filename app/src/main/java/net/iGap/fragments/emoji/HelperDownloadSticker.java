80d6d407045784b315f7f022004aa9161dbc99db
abolfazl abbasi <aabolfazlit@gmail.com>
2019-12-09 13:02:46 +0330

    ADD sticker downloader to internal storage

--- app/src/main/java/net/iGap/fragments/emoji/HelperDownloadSticker.java
+++ app/src/main/java/net/iGap/fragments/emoji/HelperDownloadSticker.java
@@ -1,18 +1,26 @@
 package net.iGap.fragments.emoji;
 
+import android.util.Log;
+
 import net.iGap.G;
+import net.iGap.eventbus.EventManager;
+import net.iGap.fragments.emoji.add.StickerDialogFragment;
+import net.iGap.fragments.emoji.add.StructIGSticker;
 import net.iGap.interfaces.OnStickerDownloaded;
 import net.iGap.proto.ProtoFileDownload;
 import net.iGap.proto.ProtoGlobal;
 import net.iGap.request.RequestFileDownload;
 
 import java.io.File;
+import java.util.HashMap;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class HelperDownloadSticker {
 
 
     public static ConcurrentHashMap<String, UpdateStickerListener> hashMap = new ConcurrentHashMap<>();
+    private static HashMap<String, String> downloadingQueue = new HashMap<>();
+    private static String TAG = "abbasiStickerDownloader";
 
     public interface UpdateStickerListener {
         void OnProgress(String path, String token, int progress);
@@ -55,6 +63,42 @@ public class HelperDownloadSticker {
         }
     }
 
+    public static void downloadSticker(StructIGSticker structIGSticker) {
+//        if (!downloadingQueue.containsKey(structIGSticker.getToken())) {
+//            downloadingQueue.put(structIGSticker.getToken(), structIGSticker.getFileName());
+        try {
+            G.onStickerDownloaded = new OnStickerDownloaded() {
+                @Override
+                public void onStickerDownloaded(String filePath, String token, long fileSize, long offset, ProtoFileDownload.FileDownload.Selector selector, RequestFileDownload.TypeDownload type, int progress) {
+//                        downloadingQueue.remove(structIGSticker.getToken());
+                    EventManager.getInstance().postEvent(EventManager.STICKER_DOWNLOAD, filePath, token);
+                }
+
+                @Override
+                public void onError(int major, Object identity) {
+                    downloadingQueue.remove(structIGSticker.getToken());
+                    Log.i(TAG, "onError: " + major + identity.toString());
+                }
+            };
+
+            new RequestFileDownload().download(structIGSticker.getToken(), 0, structIGSticker.getFileSize(), ProtoFileDownload.FileDownload.Selector.FILE,
+                    new RequestFileDownload.IdentityFileDownload(ProtoGlobal.RoomMessageType.IMAGE, structIGSticker.getToken(), structIGSticker.getPath(), ProtoFileDownload.FileDownload.Selector.FILE, structIGSticker.getFileSize(), 0, RequestFileDownload.TypeDownload.STICKER));
+
+        } catch (RuntimeException e) {
+            e.printStackTrace();
+//            }
+        }
+    }
+/*
+    public static String stickerInternalPath(StructIGSticker structIGSticker){
+        String finalPath = structIGSticker.getToken();
+        if (structIGSticker.getType() == StructIGSticker.ANIMATED_STICKER)
+            finalPath += ".json";
+        else
+            finalPath.endsWith()
+        return  finalPath;
+    }*/
+
     public static String createPathFile(String token, String extension) {
 
         String _mimeType = ".png";
@@ -63,6 +107,19 @@ public class HelperDownloadSticker {
             _mimeType = extension.substring(index);
         }
         return G.DIR_STICKER + "/" + token + _mimeType;
+    }
+
+    public static String downloadStickerPath(String token, String extension) {
+
+        String mimeType = ".png";
+        int index = extension.lastIndexOf(".");
+        if (index >= 0) {
+            mimeType = extension.substring(index);
+        }
+
+        Log.i(TAG, "createPathFile: " + StickerDialogFragment.downloadDirectoryPath + "/" + token + mimeType);
+
+        return StickerDialogFragment.downloadDirectoryPath + "/" + token + mimeType;
     }
 
 
