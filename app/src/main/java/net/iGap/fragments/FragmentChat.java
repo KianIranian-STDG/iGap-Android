1c0abfa6de8438ecaffc7001fcfad37b292f8f9e
abolfazl abbasi <aabolfazlit@gmail.com>
2019-12-16 13:39:52 +0330

    FIXED animation message type

--- app/src/main/java/net/iGap/fragments/FragmentChat.java
+++ app/src/main/java/net/iGap/fragments/FragmentChat.java
@@ -5780,19 +5780,8 @@ public class FragmentChat extends BaseFragment
                         realmAttachment.setSize(new File(st.getUri()).length());
                         realmAttachment.setName(new File(st.getUri()).getName());
                         realmAttachment.setDuration(0);
-                        // TODO: 12/16/19 must change this
-                        int type;
-                        String mimType;
 
-                        if (st.getUri().endsWith(".json")) {
-                            mimType = "text/plain";
-                            type = StructIGSticker.ANIMATED_STICKER;
-                        } else {
-                            mimType = "image/png";
-                            type = StructIGSticker.NORMAL_STICKER;
-                        }
-
-                        realmAttachment.setMimType(mimType);
+                        int type = st.getUri().endsWith(".json") ? StructIGSticker.ANIMATED_STICKER : StructIGSticker.NORMAL_STICKER;
 
                         roomMessage.setAttachment(realmAttachment);
 
@@ -5953,7 +5942,6 @@ public class FragmentChat extends BaseFragment
         realmAttachment.setSize(new File(structIGSticker.getPath()).length());
         realmAttachment.setName(new File(structIGSticker.getPath()).getName());
         realmAttachment.setDuration(0);
-        realmAttachment.setMimType("text/plain");
 
         roomMessage.setAttachment(realmAttachment);
 
@@ -7924,7 +7912,7 @@ public class FragmentChat extends BaseFragment
                         }
                         break;
                     case STICKER:
-                        if (messageInfo.realmRoomMessage.getAttachment().getMimType() != null && messageInfo.realmRoomMessage.getAttachment().getMimType().equalsIgnoreCase("text/plain")) {
+                        if (messageInfo.realmRoomMessage.getAttachment().getName() != null && messageInfo.realmRoomMessage.getAttachment().isAnimatedSticker()) {
                             if (!addTop) {
                                 mAdapter.add(new AnimatedStickerItem(mAdapter, chatType, this).setMessage(messageInfo).withIdentifier(identifier));
                             } else {
