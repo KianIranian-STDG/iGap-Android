517ed71fbe21710adf61768fd63d3954edd5abf2
a.nazari <a.nazari@kianiranian.com>
2019-12-21 15:02:00 +0330

    Fix - Fabric crash

--- app/src/main/java/net/iGap/fragments/discovery/DiscoveryFragment.java
+++ app/src/main/java/net/iGap/fragments/discovery/DiscoveryFragment.java
@@ -326,7 +326,7 @@ public class DiscoveryFragment extends BaseMainFragments implements ToolbarListe
         Gson gson = builder.create();
         SharedPreferences pref = G.context.getSharedPreferences("DiscoveryPages", Context.MODE_PRIVATE);
         String json = pref.getString("page0", "");
-        String title = pref.getString("title", "");
+        //String title = pref.getString("title", "");
         if (json != null && !json.equals("")) {
             try {
                 ArrayList<DiscoveryItem> discoveryArrayList = gson.fromJson(json, new TypeToken<ArrayList<DiscoveryItem>>() {
@@ -336,7 +336,7 @@ public class DiscoveryFragment extends BaseMainFragments implements ToolbarListe
                         return;
                     }
                 }
-                setAdapterData(discoveryArrayList, title);
+                setAdapterData(discoveryArrayList, "");
             } catch (Exception e) {
                 e.printStackTrace();
             }
