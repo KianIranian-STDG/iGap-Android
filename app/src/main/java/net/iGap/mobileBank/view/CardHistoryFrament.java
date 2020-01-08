e2473b8152cb1c68b395cd02abaee1162c96c6d9
h.amini <h.amini1993@yahoo.com>
2020-01-08 15:27:50 +0330

    Bank -> add history base + top design (NOT Complete)

---
+++ app/src/main/java/net/iGap/mobileBank/view/CardHistoryFrament.java
@@ -0,0 +1,8 @@
+package net.iGap.mobileBank.view;
+
+import net.iGap.api.apiService.BaseAPIViewFrag;
+import net.iGap.mobileBank.viewmoedel.CardHistoryViewModel;
+
+public class CardHistoryFrament extends BaseAPIViewFrag<CardHistoryViewModel> {
+
+}
---
+++ app/src/main/java/net/iGap/mobileBank/viewmoedel/CardHistoryViewModel.java
@@ -0,0 +1,7 @@
+package net.iGap.mobileBank.viewmoedel;
+
+import net.iGap.api.apiService.BaseAPIViewModel;
+
+public class CardHistoryViewModel extends BaseAPIViewModel {
+
+}
