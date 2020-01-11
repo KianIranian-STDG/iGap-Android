327805b8fc7307082b9627617a260f72806ea755
a.nazari <a.nazari@kianiranian.com>
2020-01-11 18:04:47 +0330

    Fix - Update to new UI and add slider indicators

--- app/src/main/java/net/iGap/mobileBank/view/MobileBankHomeFragment.java
+++ app/src/main/java/net/iGap/mobileBank/view/MobileBankHomeFragment.java
@@ -5,30 +5,48 @@ import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProviders;
 import androidx.viewpager.widget.ViewPager;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import net.iGap.R;
+import net.iGap.databinding.FragmentMobileBankHomeBinding;
 import net.iGap.mobileBank.repository.db.RealmMobileBankCards;
 import net.iGap.mobileBank.view.adapter.BankCardsAdapter;
+import net.iGap.mobileBank.viewmoedel.MobileBankHomeViewModel;
+import net.iGap.viewmodel.FragmentCPayViewModel;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class MobileBankHomeFragment extends Fragment {
 
+    private MobileBankHomeViewModel viewModel;
+    private FragmentMobileBankHomeBinding binding;
+
     public MobileBankHomeFragment() {
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_mobile_bank_home, container, false);
+        binding = DataBindingUtil.inflate(inflater,R.layout.fragment_mobile_bank_home, container, false);
+        binding.setLifecycleOwner(getViewLifecycleOwner());
+        binding.setVm(viewModel);
+        return binding.getRoot();
     }
 
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        viewModel = ViewModelProviders.of(this).get(MobileBankHomeViewModel.class);
+    }
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
@@ -36,13 +54,50 @@ public class MobileBankHomeFragment extends Fragment {
     }
 
     private void setupViews(View view) {
-        ViewPager viewPager = view.findViewById(R.id.vpCards);
         List<RealmMobileBankCards> cards = new ArrayList<>();
         cards.add(new RealmMobileBankCards("6221 6698 2154 4752" , "علیرضا نظری" , "02/99" , true));
         cards.add(new RealmMobileBankCards("6221 6698 3145 3456" , "حسین امینی" , "02/99" , true));
         cards.add(new RealmMobileBankCards("6221 6698 9254 6678" , "احسان زرقلمی" , "02/99" , true));
-        viewPager.setAdapter(new BankCardsAdapter(cards));
-        viewPager.setOffscreenPageLimit(cards.size() - 1);
+        binding.vpCards.setAdapter(new BankCardsAdapter(cards));
+        binding.vpCards.setOffscreenPageLimit(cards.size() - 1);
 
+        binding.vpCards.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                setEnableIndicator(position);
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+
+        createViewPagerIndicators(cards.size());
+    }
+
+    private void createViewPagerIndicators(int size) {
+        for (int i=0 ; i < size ; i++){
+            ImageView iv = new ImageView(binding.lytIndicators.getContext());
+            LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(28 , 28);
+            lp.setMargins(6 , 6 , 6 , 6 );
+            iv.setLayoutParams(lp);
+            iv.setBackgroundResource(R.drawable.indicator_slider);
+            if (i == 0) iv.setSelected(true);
+            binding.lytIndicators.addView(iv);
+        }
+    }
+
+    private void setEnableIndicator(int position){
+        int count = binding.lytIndicators.getChildCount();
+        if (position > count) return;
+        for (int i=0 ; i < count ; i++){
+            binding.lytIndicators.getChildAt(i).setSelected(i == position);
+        }
     }
 }
--- app/src/main/java/net/iGap/mobileBank/viewmoedel/MobileBankHomeViewModel.java
+++ app/src/main/java/net/iGap/mobileBank/viewmoedel/MobileBankHomeViewModel.java
@@ -3,4 +3,9 @@ package net.iGap.mobileBank.viewmoedel;
 import net.iGap.api.apiService.BaseAPIViewModel;
 
 public class MobileBankHomeViewModel extends BaseAPIViewModel {
+
+    public void OnTransferMoneyClicked(){}
+    public void OnTransactionsClicked(){}
+    public void OnTemporaryPassClicked(){}
+    public void OnShebaClicked(){}
 }
