6c12781b043553d5a2765f5c3628dc027ab43033
a.nazari <a.nazari@kianiranian.com>
2020-01-12 17:45:13 +0330

    Improve - Add Transfer type dialog picker and change codes

--- app/src/main/java/net/iGap/mobileBank/viewmoedel/MobileBankHomeViewModel.java
+++ app/src/main/java/net/iGap/mobileBank/viewmoedel/MobileBankHomeViewModel.java
@@ -6,12 +6,17 @@ import net.iGap.module.SingleLiveEvent;
 public class MobileBankHomeViewModel extends BaseAPIViewModel {
 
     public SingleLiveEvent<Boolean> onMoneyTransferListener = new SingleLiveEvent<>();
+    public SingleLiveEvent<Boolean> onTempPassListener = new SingleLiveEvent<>();
 
     public void OnTransferMoneyClicked(){
         onMoneyTransferListener.setValue(true);
     }
 
     public void OnTransactionsClicked(){}
-    public void OnTemporaryPassClicked(){}
+
+    public void OnTemporaryPassClicked(){
+        onTempPassListener.postValue(true);
+    }
+
     public void OnShebaClicked(){}
 }
