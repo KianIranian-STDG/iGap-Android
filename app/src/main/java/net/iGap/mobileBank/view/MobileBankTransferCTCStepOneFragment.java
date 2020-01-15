bfb58df388c750cc3e5ad561fa52acd8c5b940cf
h.amini <h.amini1993@yahoo.com>
2020-01-15 09:51:31 +0330

    Bank -> card to card step 1

--- app/src/main/java/net/iGap/mobileBank/view/MobileBankTransferCTCStepOneFragment.java
+++ app/src/main/java/net/iGap/mobileBank/view/MobileBankTransferCTCStepOneFragment.java
@@ -1,19 +1,28 @@
 package net.iGap.mobileBank.view;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.widget.AppCompatEditText;
 import androidx.databinding.DataBindingUtil;
 import androidx.lifecycle.ViewModelProviders;
 
 import net.iGap.R;
 import net.iGap.api.apiService.BaseAPIViewFrag;
 import net.iGap.databinding.MobileBankTransferCtcStepOneBinding;
+import net.iGap.helper.HelperToolbar;
+import net.iGap.interfaces.ToolbarListener;
 import net.iGap.mobileBank.viewmoedel.MobileBankTransferCTCStepOneViewModel;
+
+import java.util.Locale;
 
 public class MobileBankTransferCTCStepOneFragment extends BaseAPIViewFrag<MobileBankTransferCTCStepOneViewModel> {
 
@@ -41,5 +50,333 @@ public class MobileBankTransferCTCStepOneFragment extends BaseAPIViewFrag<Mobile
 
         return attachToSwipeBack(binding.getRoot());
 
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+
+        super.onViewCreated(view, savedInstanceState);
+
+        HelperToolbar mHelperToolbar = HelperToolbar.create()
+                .setContext(getContext())
+                .setLifecycleOwner(getViewLifecycleOwner())
+                .setLeftIcon(R.string.back_icon)
+                .setListener(new ToolbarListener() {
+                    @Override
+                    public void onLeftIconClickListener(View view) {
+                        popBackStackFragment();
+                    }
+                })
+                .setLogoShown(true);
+
+        LinearLayout toolbarLayout = binding.Toolbar;
+        toolbarLayout.addView(mHelperToolbar.getView());
+
+        binding.originCard.cardNumberField1.requestFocus();
+
+        textInputManagerOrigin();
+        textInputManagerDest();
+        textInputManagerValue();
+    }
+
+    private void textInputManagerValue() {
+        binding.amount.addTextChangedListener(new TextWatcher() {
+            boolean isSettingText;
+            String mPrice;
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                mPrice = s.toString().replaceAll(",", "");
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if (isSettingText) return;
+
+                isSettingText = true;
+                String s1 = null;
+
+                try {
+                    s1 = String.format(Locale.US, "%,d", Long.parseLong(mPrice));
+                } catch (NumberFormatException e) {
+                    e.printStackTrace();
+                }
+
+                binding.amount.setText(s1);
+                binding.amount.setSelection(binding.amount.length());
+
+                isSettingText = false;
+                // ?????????
+                /*String amount = s.toString().replace(",","");
+                DecimalFormat df = new DecimalFormat(",###");
+                binding.amount.setText(df.format(Integer.parseInt(amount)));*/
+                viewModel.setAmount(s.toString().replace(",", ""));
+            }
+        });
+    }
+
+    private void textInputManagerOrigin() {
+        // Pin 1
+        binding.originCard.cardNumberField1.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if (s.length() == 4) {
+                    binding.originCard.cardNumberField2.setEnabled(true);
+                    binding.originCard.cardNumberField2.requestFocus();
+                }
+            }
+        });
+
+        // Pin 2
+        binding.originCard.cardNumberField2.setOnKeyListener((v, keyCode, event) -> {
+            if (event.getAction() == KeyEvent.ACTION_DOWN && keyCode == KeyEvent.KEYCODE_DEL) {
+                if (((AppCompatEditText) v).getEditableText().length() == 0) {
+                    binding.originCard.cardNumberField1.requestFocus();
+                    binding.originCard.cardNumberField2.setEnabled(false);
+                    return true;
+                }
+            }
+            return false;
+        });
+        binding.originCard.cardNumberField2.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if (s.length() > 1) {
+                    String cardNum = binding.originCard.cardNumberField1.getText().toString()
+                            + binding.originCard.cardNumberField2.getText().toString();
+                    viewModel.setOriginCard(cardNum);
+                } else {
+                    viewModel.setOriginCard(null);
+                }
+                if (s.length() == 4) {
+                    binding.originCard.cardNumberField3.setEnabled(true);
+                    binding.originCard.cardNumberField3.requestFocus();
+                }
+            }
+        });
+
+        // Pin 3
+        binding.originCard.cardNumberField3.setOnKeyListener((v, keyCode, event) -> {
+            if (event.getAction() == KeyEvent.ACTION_DOWN && keyCode == KeyEvent.KEYCODE_DEL) {
+                if (((AppCompatEditText) v).getEditableText().length() == 0) {
+                    binding.originCard.cardNumberField2.requestFocus();
+                    binding.originCard.cardNumberField3.setEnabled(false);
+                    return true;
+                }
+            }
+            return false;
+        });
+        binding.originCard.cardNumberField3.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if (s.length() == 4) {
+                    binding.originCard.cardNumberField4.setEnabled(true);
+                    binding.originCard.cardNumberField4.requestFocus();
+                }
+            }
+        });
+
+        // Pin 4
+        binding.originCard.cardNumberField4.setOnKeyListener((v, keyCode, event) -> {
+            if (event.getAction() == KeyEvent.ACTION_DOWN && keyCode == KeyEvent.KEYCODE_DEL) {
+                String cardNum = binding.originCard.cardNumberField1.getText().toString()
+                        + binding.originCard.cardNumberField2.getText().toString();
+                viewModel.setCompleteOrigin(false, cardNum);
+                if (((AppCompatEditText) v).getEditableText().length() == 0) {
+                    binding.originCard.cardNumberField3.requestFocus();
+                    binding.originCard.cardNumberField4.setEnabled(false);
+                    return true;
+                }
+            }
+            return false;
+        });
+        binding.originCard.cardNumberField4.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if (s.length() == 4) {
+                    // build the Pin Code
+                    String cardNum = binding.originCard.cardNumberField1.getText().toString()
+                            + binding.originCard.cardNumberField2.getText().toString()
+                            + binding.originCard.cardNumberField3.getText().toString()
+                            + binding.originCard.cardNumberField4.getText().toString();
+                    viewModel.setCompleteOrigin(true, cardNum);
+                }
+            }
+        });
+    }
+
+    private void textInputManagerDest() {
+        // Pin 1
+        binding.destCard.cardNumberField1.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if (s.length() == 4) {
+                    binding.destCard.cardNumberField2.setEnabled(true);
+                    binding.destCard.cardNumberField2.requestFocus();
+                }
+            }
+        });
+
+        // Pin 2
+        binding.destCard.cardNumberField2.setOnKeyListener((v, keyCode, event) -> {
+            if (event.getAction() == KeyEvent.ACTION_DOWN && keyCode == KeyEvent.KEYCODE_DEL) {
+                if (((AppCompatEditText) v).getEditableText().length() == 0) {
+                    binding.destCard.cardNumberField1.requestFocus();
+                    binding.destCard.cardNumberField2.setEnabled(false);
+                    return true;
+                }
+            }
+            return false;
+        });
+        binding.destCard.cardNumberField2.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if (s.length() > 1) {
+                    String cardNum = binding.destCard.cardNumberField1.getText().toString()
+                            + binding.destCard.cardNumberField2.getText().toString();
+                    viewModel.setDestCard(cardNum);
+                } else {
+                    viewModel.setDestCard(null);
+                }
+                if (s.length() == 4) {
+                    binding.destCard.cardNumberField3.setEnabled(true);
+                    binding.destCard.cardNumberField3.requestFocus();
+                }
+            }
+        });
+
+        // Pin 3
+        binding.destCard.cardNumberField3.setOnKeyListener((v, keyCode, event) -> {
+            if (event.getAction() == KeyEvent.ACTION_DOWN && keyCode == KeyEvent.KEYCODE_DEL) {
+                if (((AppCompatEditText) v).getEditableText().length() == 0) {
+                    binding.destCard.cardNumberField2.requestFocus();
+                    binding.destCard.cardNumberField3.setEnabled(false);
+                    return true;
+                }
+            }
+            return false;
+        });
+        binding.destCard.cardNumberField3.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if (s.length() == 4) {
+                    binding.destCard.cardNumberField4.setEnabled(true);
+                    binding.destCard.cardNumberField4.requestFocus();
+                }
+            }
+        });
+
+        // Pin 4
+        binding.destCard.cardNumberField4.setOnKeyListener((v, keyCode, event) -> {
+            if (event.getAction() == KeyEvent.ACTION_DOWN && keyCode == KeyEvent.KEYCODE_DEL) {
+                String cardNum = binding.destCard.cardNumberField1.getText().toString()
+                        + binding.destCard.cardNumberField2.getText().toString();
+                viewModel.setCompleteDest(false, cardNum);
+                if (((AppCompatEditText) v).getEditableText().length() == 0) {
+                    binding.destCard.cardNumberField3.requestFocus();
+                    binding.destCard.cardNumberField4.setEnabled(false);
+                    return true;
+                }
+            }
+            return false;
+        });
+        binding.destCard.cardNumberField4.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if (s.length() == 4) {
+                    // build the Pin Code
+                    String cardNum = binding.destCard.cardNumberField1.getText().toString()
+                            + binding.destCard.cardNumberField2.getText().toString()
+                            + binding.destCard.cardNumberField3.getText().toString()
+                            + binding.destCard.cardNumberField4.getText().toString();
+                    viewModel.setCompleteDest(true, cardNum);
+                }
+            }
+        });
     }
 }
--- app/src/main/java/net/iGap/mobileBank/viewmoedel/MobileBankTransferCTCStepOneViewModel.java
+++ app/src/main/java/net/iGap/mobileBank/viewmoedel/MobileBankTransferCTCStepOneViewModel.java
@@ -1,6 +1,190 @@
 package net.iGap.mobileBank.viewmoedel;
 
+import android.view.View;
+
+import androidx.databinding.ObservableField;
+
 import net.iGap.api.apiService.BaseAPIViewModel;
 
 public class MobileBankTransferCTCStepOneViewModel extends BaseAPIViewModel {
+
+    private ObservableField<Integer> originCardNameVisibility = new ObservableField<>(View.GONE);
+    private ObservableField<Integer> destCardNameVisibility = new ObservableField<>(View.GONE);
+    private ObservableField<Integer> originBankVisibility = new ObservableField<>(View.INVISIBLE);
+    private ObservableField<Integer> destBankVisibility = new ObservableField<>(View.INVISIBLE);
+    private ObservableField<Integer> amountTextVisibility = new ObservableField<>(View.INVISIBLE);
+
+    private ObservableField<String> amountTranslate = new ObservableField<>("");
+    private ObservableField<String> amountEntry = new ObservableField<>("");
+    private ObservableField<String> originCardName = new ObservableField<>("Fetching data...");
+    private ObservableField<String> destCardName = new ObservableField<>("Fetching data...");
+
+    private Boolean completeOrigin = false;
+    private Boolean completeDest = false;
+    private String originCard = null;
+    private String destCard = null;
+    private String amount = null;
+
+    private void getCardInfo(String cardNum, boolean isOrigin) {
+        // api call
+    }
+
+    private boolean getCardBank(String cardNum) {
+        // bank logo
+        return cardNum != null;
+    }
+
+    private void translateValue() {
+        // show text of amount
+        if (amount != null)
+            amountTextVisibility.set(View.VISIBLE);
+        else
+            amountTextVisibility.set(View.INVISIBLE);
+
+        if (amount.length() > 10) {
+            amountTranslate.set("error");
+            return;
+        }
+
+        if (amount.length() == 0) {
+            amountTranslate.set("");
+            return;
+        }
+
+        String translate = "";
+        int position = 0;
+        if (amount.charAt(amount.length() - 1) != '0')
+            translate = " و " + amount.charAt(amount.length() - 1) + getCurrency(position);
+
+        if (amount.length() > 1) {
+            int original;
+            if (amount.length() == 2)
+                original = Integer.parseInt("" + amount.charAt(0));
+            else
+                original = Integer.parseInt(amount.substring(0, amount.length() - 1));
+
+            while (original != 0) {
+                position++;
+                if (original % 1000 != 0) {
+                    /*if (translate.length() != 0)
+                        translate = " و" + translate;*/
+                    translate = " و " + original % 1000 + getCurrency(position) + translate;
+                } else {
+                    if (position == 1)
+                        translate = getCurrency(position) + translate;
+                }
+                original = original / 1000;
+            }
+        }
+        translate = translate.substring(2);
+        amountTranslate.set(translate);
+    }
+
+    private String getCurrency(int position) {
+        switch (position) {
+            case 0:
+                return " ریال";
+            case 1:
+                return " تومان";
+            case 2:
+                return " هزار";
+            case 3:
+                return " میلیون";
+            case 4:
+                return " میلیارد";
+            default:
+                return "";
+        }
+    }
+
+    public ObservableField<Integer> getOriginCardNameVisibility() {
+        return originCardNameVisibility;
+    }
+
+    public ObservableField<Integer> getDestCardNameVisibility() {
+        return destCardNameVisibility;
+    }
+
+    public ObservableField<Integer> getAmountTextVisibility() {
+        return amountTextVisibility;
+    }
+
+    public void setCompleteOrigin(Boolean completeOrigin, String cardNum) {
+        this.completeOrigin = completeOrigin;
+        if (completeOrigin) {
+            originCardNameVisibility.set(View.VISIBLE);
+            originCard = cardNum;
+            getCardInfo(cardNum, true);
+        } else {
+            originCardNameVisibility.set(View.GONE);
+            originCard = cardNum.substring(0, 5);
+        }
+    }
+
+    public void setCompleteDest(Boolean completeDest, String cardNum) {
+        this.completeDest = completeDest;
+        if (completeDest) {
+            destCardNameVisibility.set(View.VISIBLE);
+            destCard = cardNum;
+            getCardInfo(cardNum, false);
+        } else {
+            destCardNameVisibility.set(View.GONE);
+            destCard = cardNum.substring(0, 5);
+        }
+    }
+
+    public void setOriginCard(String originCard) {
+        this.originCard = originCard;
+        if (getCardBank(originCard)) {
+            originBankVisibility.set(View.VISIBLE);
+        } else {
+            originBankVisibility.set(View.INVISIBLE);
+        }
+    }
+
+    public void setDestCard(String destCard) {
+        this.destCard = destCard;
+        if (getCardBank(destCard)) {
+            destBankVisibility.set(View.VISIBLE);
+        } else {
+            destBankVisibility.set(View.INVISIBLE);
+        }
+    }
+
+    public void setAmount(String amount) {
+        this.amount = amount;
+        translateValue();
+    }
+
+    public ObservableField<Integer> getOriginBankVisibility() {
+        return originBankVisibility;
+    }
+
+    public ObservableField<Integer> getDestBankVisibility() {
+        return destBankVisibility;
+    }
+
+    public ObservableField<String> getAmountTranslate() {
+        return amountTranslate;
+    }
+
+    public ObservableField<String> getOriginCardName() {
+        return originCardName;
+    }
+
+    public ObservableField<String> getDestCardName() {
+        return destCardName;
+    }
+
+    public ObservableField<String> getAmountEntry() {
+        return amountEntry;
+    }
+
+    public void setAmountEntry(ObservableField<String> amountEntry) {
+        this.amountEntry = amountEntry;
+    }
+
+    public String getAmount() {
+        return amount;
+    }
 }
