162dc2a495c6cf9062f55f2adf8a0af30597c543
Mehdi.Bagvand <m.bagvand@kianiranian.com>
2020-01-19 21:13:11 +0330

    implement giftStickerItem.

--- app/src/main/java/net/iGap/adapter/items/chat/GiftStickerItem.java
+++ app/src/main/java/net/iGap/adapter/items/chat/GiftStickerItem.java
@@ -111,14 +111,21 @@ public class GiftStickerItem extends AbstractMessage<GiftStickerItem, GiftSticke
     protected class ViewHolder extends NewChatItemHolder implements IProgress, IThumbNailItem {
         protected AppCompatImageView image;
         protected MessageProgress progress;
-        protected Button visitBtn;
-        protected Button addToArchiveBtn;
+        private Button visitBtn;
+        private Button addToArchiveBtn;
+        private AppCompatTextView cardNumber1;
+        private AppCompatTextView cardNumber2;
+        private AppCompatTextView cardNumber3;
+        private AppCompatTextView cardNumber4;
+        private AppCompatTextView cvv2Tv;
+        private AppCompatTextView expireTimeTv;
 
 
         public ViewHolder(View view) {
             super(view);
 
             LinearLayout contentOne = new LinearLayout(getContext());
+            contentOne.setVisibility(View.GONE);
             contentOne.setOrientation(LinearLayout.VERTICAL);
 
             image = new AppCompatImageView(getContext());
@@ -159,27 +166,79 @@ public class GiftStickerItem extends AbstractMessage<GiftStickerItem, GiftSticke
             progress = getProgressBar(view.getContext(), 0);
             contentOne.addView(progress);
 
-
             LinearLayout contentTwo = new LinearLayout(getContext());
-            contentTwo.setVisibility(View.GONE);
+            contentTwo.setPadding(dpToPx(10), dpToPx(10), dpToPx(10), dpToPx(10));
             contentTwo.setOrientation(LinearLayout.VERTICAL);
 
             AppCompatTextView giftCardTv = new AppCompatTextView(getContext());
+            LinearLayout.LayoutParams giftCardTvLayoutParams = new LinearLayout.LayoutParams(Resources.getSystem().getDisplayMetrics().widthPixels, LinearLayout.LayoutParams.WRAP_CONTENT);
+            giftCardTvLayoutParams.gravity = Gravity.CENTER;
+            giftCardTv.setLayoutParams(giftCardTvLayoutParams);
+
             giftCardTv.setText(R.string.gift_cart_title);
             giftCardTv.setGravity(Gravity.CENTER);
             setTextSize(giftCardTv, R.dimen.largeTextSize);
-            giftCardTv.setPadding(dpToPx(2), 0, dpToPx(2), 0);
+            giftCardTv.setPadding(dpToPx(2), dpToPx(15), dpToPx(2), dpToPx(15));
             giftCardTv.setSingleLine(true);
             setTypeFace(giftCardTv);
 
             LinearLayout cardNumberLayout = new LinearLayout(getContext());
             buttonLayouts.setOrientation(LinearLayout.HORIZONTAL);
 
+            cardNumber1 = generateCardNumberTv();
+            cardNumber2 = generateCardNumberTv();
+            cardNumber3 = generateCardNumberTv();
+            cardNumber4 = generateCardNumberTv();
+
+            cardNumberLayout.addView(cardNumber1);
+            cardNumberLayout.addView(cardNumber2);
+            cardNumberLayout.addView(cardNumber3);
+            cardNumberLayout.addView(cardNumber4);
+
+            LinearLayout bottomLayout = new LinearLayout(getContext());
+            buttonLayouts.setOrientation(LinearLayout.HORIZONTAL);
+
+
+            cvv2Tv = new AppCompatTextView(getContext());
+            cvv2Tv.setLayoutParams(LayoutCreator.createLinear(0, LayoutCreator.WRAP_CONTENT, 1, Gravity.CENTER));
+            cvv2Tv.setText("----");
+            cvv2Tv.setGravity(Gravity.LEFT);
+            setTextSize(cvv2Tv, R.dimen.largeTextSize);
+            cvv2Tv.setPadding(dpToPx(15), 0, dpToPx(2), 0);
+            cvv2Tv.setSingleLine(true);
+            setTypeFace(cvv2Tv);
+
+            expireTimeTv = new AppCompatTextView(getContext());
+            expireTimeTv.setLayoutParams(LayoutCreator.createLinear(0, LayoutCreator.WRAP_CONTENT, 1, Gravity.CENTER));
+            expireTimeTv.setText("----");
+            expireTimeTv.setGravity(Gravity.RIGHT);
+            setTextSize(expireTimeTv, R.dimen.largeTextSize);
+            expireTimeTv.setPadding(dpToPx(2), 0, dpToPx(15), 0);
+            expireTimeTv.setSingleLine(true);
+            setTypeFace(expireTimeTv);
+
+            bottomLayout.addView(cvv2Tv);
+            bottomLayout.addView(expireTimeTv);
+
             contentTwo.addView(giftCardTv);
-            cardNumberLayout.addView(cardNumberLayout);
+            contentTwo.addView(cardNumberLayout);
+            contentTwo.addView(bottomLayout);
 
+            contentTwo.setBackgroundResource(theme.getCardToCardButtonBackground(contentTwo.getContext()));
             getContentBloke().addView(contentTwo);
+        }
 
+        private AppCompatTextView generateCardNumberTv() {
+            AppCompatTextView cardNumber = new AppCompatTextView(getContext());
+            cardNumber.setLayoutParams(LayoutCreator.createLinear(0, LayoutCreator.WRAP_CONTENT, 1, Gravity.CENTER));
+            cardNumber.setText("----");
+            cardNumber.setGravity(Gravity.CENTER);
+            setTextSize(cardNumber, R.dimen.largeTextSize);
+            cardNumber.setPadding(dpToPx(2), 0, dpToPx(2), 0);
+            cardNumber.setSingleLine(true);
+            setTypeFace(cardNumber);
+
+            return cardNumber;
         }
 
         @Override
