355ac39a90c214126b99182cae6d3de023bd01df
mozafari <mozafari7234>
2020-01-19 11:09:42 +0330

    Improvement - Discovery Alert added default message for discovery

--- app/src/main/java/net/iGap/adapter/items/discovery/holder/BaseViewHolder.java
+++ app/src/main/java/net/iGap/adapter/items/discovery/holder/BaseViewHolder.java
@@ -60,7 +60,6 @@ import net.iGap.interfaces.OnGeoGetConfiguration;
 import net.iGap.interfaces.OnGetPermission;
 import net.iGap.internetpackage.BuyInternetPackageFragment;
 import net.iGap.kuknos.view.KuknosEntryOptionFrag;
-import net.iGap.kuknos.view.KuknosLoginFrag;
 import net.iGap.model.MciPurchaseResponse;
 import net.iGap.module.SHP_SETTING;
 import net.iGap.news.view.NewsMainFrag;
@@ -363,7 +362,9 @@ public abstract class BaseViewHolder extends RecyclerView.ViewHolder {
                     e.printStackTrace();
                 }
                 break;
-
+            default:
+                new MaterialDialog.Builder(activity).content(R.string.install_latest_version).positiveText(R.string.ok).onPositive((dialog, which) -> dialog.dismiss()).show();
+                break;
         }
     }
 
