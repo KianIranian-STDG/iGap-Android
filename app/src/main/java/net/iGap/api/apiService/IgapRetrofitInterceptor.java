071c9f2c607b0d0c868d64274a005d438ca0b215
abolfazl abbasi <aabolfazlit@gmail.com>
2020-02-19 11:42:41 +0330

    added client specifications in every request headers

--- app/src/main/java/net/iGap/api/apiService/IgapRetrofitInterceptor.java
+++ app/src/main/java/net/iGap/api/apiService/IgapRetrofitInterceptor.java
@@ -1,5 +1,9 @@
 package net.iGap.api.apiService;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+
+import net.iGap.BuildConfig;
 import net.iGap.G;
 
 import org.jetbrains.annotations.NotNull;
@@ -11,12 +15,24 @@ import okhttp3.Request;
 import okhttp3.Response;
 
 public class IgapRetrofitInterceptor implements Interceptor {
+    private String specifications;
+
+    IgapRetrofitInterceptor() {
+        JsonObject jsonObject = new JsonObject();
+        String appId = "2";
+        jsonObject.addProperty("appId", appId);
+        String appVersion = String.valueOf(BuildConfig.VERSION_CODE);
+        jsonObject.addProperty("appVersion", appVersion);
+        specifications = new Gson().toJson(jsonObject);
+    }
+
     @NotNull
     @Override
     public Response intercept(Chain chain) throws IOException {
         Request original = chain.request();
         Request request = original.newBuilder()
                 .header("Authorization", G.getApiToken())
+                .header("specifications", specifications)
                 .header("Content-Type", "application/json")
                 .method(original.method(), original.body())
                 .build();
