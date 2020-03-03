0e126d2b72cc05a96346de79f710c54fe8b569c7
abolfazl abbasi <a.abbasi@kianiranian.com>
2020-03-03 15:18:13 +0330

    fixed pagination in room list

--- app/src/main/java/net/iGap/response/ClientGetRoomListResponse.java
+++ app/src/main/java/net/iGap/response/ClientGetRoomListResponse.java
@@ -78,14 +78,15 @@ public class ClientGetRoomListResponse extends MessageHandler {
         FragmentMain.mOffset += clientGetRoomListResponse.getRoomsCount();
         isPendingGetRoomList = false;
         if (clientGetRoomListResponse.getRoomsCount() == 0) {
-            if (G.onClientGetRoomListResponse != null) {
-                G.onClientGetRoomListResponse.onClientGetRoomList(null, null, null);
-            }
             roomListFetched = true;
         }/* else {
             boolean send = new RequestClientGetRoomList().clientGetRoomList(FragmentMain.mOffset, Config.LIMIT_LOAD_ROOM, "");
 
         }*/
+
+        if (G.onClientGetRoomListResponse != null) {
+            G.onClientGetRoomListResponse.onClientGetRoomList(null, null, null);
+        }
 
         retryCountZeroOffset = 0;
     }
