5bcd894af42b3a470d73b8a571b84cd944085c2f
shayan moradi <shayan.mwp@gmail.com>
2020-04-12 14:50:11 +0430

    changing weaverview now affects voice before playing music elapsed time and total time on changing weaverview before palying voice fixed

--- app/src/main/java/net/iGap/adapter/items/chat/VoiceItem.java
+++ app/src/main/java/net/iGap/adapter/items/chat/VoiceItem.java
@@ -11,6 +11,7 @@
 package net.iGap.adapter.items.chat;
 
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.MotionEvent;
 import android.view.View;
@@ -21,6 +22,7 @@ import androidx.appcompat.widget.AppCompatTextView;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.constraintlayout.widget.ConstraintSet;
 
+import net.iGap.module.AndroidUtils;
 import net.iGap.module.accountManager.DbManager;
 import net.iGap.G;
 import net.iGap.R;
@@ -46,7 +48,11 @@ import java.io.File;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
+import yogesh.firzen.mukkiasevaigal.S;
+
+import static java.lang.Long.min;
 import static java.lang.Long.parseLong;
 
 public class VoiceItem extends AbstractMessage<VoiceItem, VoiceItem.ViewHolder> {
@@ -131,12 +137,10 @@ public class VoiceItem extends AbstractMessage<VoiceItem, VoiceItem.ViewHolder> 
 
         holder.btnPlayMusic.setOnClickListener(v -> {
 
-
             if (FragmentChat.isInSelectionMode) {
                 holder.itemView.performLongClick();
                 return;
             }
-
             if (holder.mFilePath.length() < 1)
                 return;
 
@@ -147,6 +151,9 @@ public class VoiceItem extends AbstractMessage<VoiceItem, VoiceItem.ViewHolder> 
                 RealmClientCondition.addOfflineListen(holder.mRoomId, parseLong(holder.mMessageID));
             }
 
+            AndroidUtils.getAudioDuration(G.context, holder.mFilePath);
+            int currentVoiceGoTO = (int) (AndroidUtils.getAudioDuration(G.context, holder.mFilePath) * holder.waveView.getProgress() / 100);
+            MusicPlayer.currentDuration = currentVoiceGoTO;
             if (holder.mMessageID.equals(MusicPlayer.messageId)) {
                 MusicPlayer.onCompleteChat = holder.complete;
 
@@ -174,6 +181,15 @@ public class VoiceItem extends AbstractMessage<VoiceItem, VoiceItem.ViewHolder> 
                 }
                 if (holder.mMessageID.equals(MusicPlayer.messageId)) {
                     MusicPlayer.setMusicProgress((int) holder.waveView.getProgress());
+                }
+            }
+            if (event.getAction() == MotionEvent.ACTION_UP || event.getAction() == MotionEvent.ACTION_CANCEL) {
+                int currentVoiceGoTO = (int) (AndroidUtils.getAudioDuration(G.context, holder.mFilePath) * holder.waveView.getProgress() / 100);
+                String finalElapsedTime = exractTimingInString(currentVoiceGoTO);
+                String totalSizeInString = exractTimingInString((int) AndroidUtils.getAudioDuration(G.context, holder.mFilePath));
+                holder.txt_Timer.setText(finalElapsedTime + holder.getContext().getString(R.string.forward_slash) + totalSizeInString);
+                if (HelperCalander.isPersianUnicode) {
+                    holder.txt_Timer.setText(HelperCalander.convertToUnicodeFarsiNumber(holder.txt_Timer.getText().toString()));
                 }
             }
             return false;
@@ -405,5 +421,22 @@ public class VoiceItem extends AbstractMessage<VoiceItem, VoiceItem.ViewHolder> 
         public MessageProgress getProgress() {
             return progress;
         }
+    }
+
+    private String exractTimingInString(int currentVoiceGoTO) {
+        int timeToSec = currentVoiceGoTO / 1000;
+        long minutes = TimeUnit.MILLISECONDS.toMinutes(currentVoiceGoTO);
+        long sec = timeToSec % 60;
+
+        String minTo = minutes + "";
+        String secTo = sec + "";
+        if (sec < 10) {
+            secTo = "0" + secTo;
+        }
+        if (minutes < 10) {
+            minTo = "0" + minTo;
+        }
+        String finalElapsedTime = minTo + ":" + secTo;
+        return finalElapsedTime;
     }
 }
