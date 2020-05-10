eb2d3e23973ba492a2fa965286e2d52dc25a0af5
abolfazl abbasi <aabolfazlit@gmail.com>
2020-05-10 10:03:06 +0430

    added incoming and call service notification

---
+++ app/src/main/java/net/iGap/module/CallActionsReceiver.java
@@ -0,0 +1,14 @@
+package net.iGap.module;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class CallActionsReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if (CallService.getInstance() != null) {
+
+        }
+    }
+}
