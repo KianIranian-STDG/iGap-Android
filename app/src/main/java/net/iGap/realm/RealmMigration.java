40d8d6059915562c6d02431f508f20b08d8222ba
abolfazl abbasi <aabolfazlit@gmail.com>
2020-07-26 15:27:07 +0430

    changed RealmMigration

--- app/src/main/java/net/iGap/realm/RealmMigration.java
+++ app/src/main/java/net/iGap/realm/RealmMigration.java
@@ -15,6 +15,8 @@ import net.iGap.model.AccountUser;
 import net.iGap.model.PassCode;
 import net.iGap.module.accountManager.AccountManager;
 
+import javax.activation.MimetypesFileTypeMap;
+
 import io.realm.DynamicRealm;
 import io.realm.DynamicRealmObject;
 import io.realm.FieldAttribute;
@@ -876,11 +878,20 @@ public class RealmMigration implements io.realm.RealmMigration {
         }
 
         if (oldVersion == 48) {
-
             RealmObjectSchema realmAttachment = schema.get(RealmAttachment.class.getSimpleName());
-
             if (realmAttachment != null) {
                 realmAttachment.addField("mimeType", String.class);
+
+                RealmResults<DynamicRealmObject> realmAttachments = realm.where(RealmAttachment.class.getSimpleName()).findAll();
+                for (DynamicRealmObject attachment : realmAttachments) {
+                    if (attachment != null) {
+                        String fileName = attachment.getString("name");
+                        if (fileName != null) {
+                            String mimeType = new MimetypesFileTypeMap().getContentType(fileName);
+                            attachment.setString("mimeType", mimeType);
+                        }
+                    }
+                }
             }
 
             oldVersion++;
