fa3888ce047c83c7bf272bd5e22f743b9630ec99
nazanin <n.omrani>
2020-09-16 12:52:48 +0430

    added new address

--- app/src/main/java/net/iGap/api/IgashtApi.java
+++ app/src/main/java/net/iGap/api/IgashtApi.java
@@ -1,14 +1,14 @@
 package net.iGap.api;
 
 import net.iGap.model.igasht.BaseIGashtResponse;
-import net.iGap.model.igasht.TicketQRCodeResponse;
-import net.iGap.model.igasht.IGashtTicketDetail;
-import net.iGap.model.igasht.TicketHistoryListResponse;
-import net.iGap.model.igasht.RegisterTicketResponse;
+import net.iGap.model.igasht.IGashtLocationItem;
 import net.iGap.model.igasht.IGashtLocationService;
 import net.iGap.model.igasht.IGashtOrder;
-import net.iGap.model.igasht.IGashtLocationItem;
 import net.iGap.model.igasht.IGashtProvince;
+import net.iGap.model.igasht.IGashtTicketDetail;
+import net.iGap.model.igasht.RegisterTicketResponse;
+import net.iGap.model.igasht.TicketHistoryListResponse;
+import net.iGap.model.igasht.TicketQRCodeResponse;
 
 import retrofit2.Call;
 import retrofit2.http.Body;
@@ -19,22 +19,22 @@ import retrofit2.http.Query;
 
 public interface IgashtApi {
 
-    @GET("province")
+    @GET("entertainment/province")
     Call<BaseIGashtResponse<IGashtProvince>> requestGetProvinceList();
 
-    @GET("location/{provinceId}")
+    @GET("entertainment/location/{provinceId}")
     Call<BaseIGashtResponse<IGashtLocationItem>> requestGetLocationList(@Path("provinceId") int provinceId);
 
-    @GET("service/{locationId}")
+    @GET("entertainment/service/{locationId}")
     Call<BaseIGashtResponse<IGashtLocationService>> requestGetServiceList(@Path("locationId") int locationId);
 
-    @POST("purchase")
+    @POST("entertainment/purchase")
     Call<RegisterTicketResponse> registerOrder(@Body IGashtOrder order);
 
-    @GET("ticket/list")
+    @GET("entertainment/ticket/list")
     Call<TicketHistoryListResponse<IGashtTicketDetail>> requestGetTicketList(@Query("offet") int offset,
                                                                              @Query("limit") int limit);
 
-    @GET("ticket/qr-code/{voucher_number}")
+    @GET("entertainment/ticket/qr-code/{voucher_number}")
     Call<TicketQRCodeResponse> requestGetTicketQRCode(@Path("voucher_number") String voucherNumber);
 }
