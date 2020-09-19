25361684a460bfd3404ffbaf7543e0d561ca8021
nazanin <n.omrani>
2020-09-19 14:45:26 +0430

    changed model

--- app/src/main/java/net/iGap/model/igasht/ExtraDetail.java
+++ app/src/main/java/net/iGap/model/igasht/ExtraDetail.java
@@ -2,6 +2,8 @@ package net.iGap.model.igasht;
 
 import com.google.gson.annotations.SerializedName;
 
+import java.util.List;
+
 @SuppressWarnings("unused")
 public class ExtraDetail {
 
@@ -11,14 +13,17 @@ public class ExtraDetail {
     private String mFullDescription;
     @SerializedName("ShortDescription")
     private String mShortDescription;
-    @SerializedName("Gallery_1")
-    private String mGallery1;
-    @SerializedName("Gallery_2")
-    private String mGallery2;
-    @SerializedName("Gallery_3")
-    private String mGallery3;
-    @SerializedName("Gallery_4")
-    private String mGallery4;
+
+    public List<Gallery> getmGallery() {
+        return mGallery;
+    }
+
+    public void setmGallery(List<Gallery> mGallery) {
+        this.mGallery = mGallery;
+    }
+
+    @SerializedName("Gallery")
+    private List<Gallery> mGallery;
 
 
     public String getmCoverImage() {
@@ -35,38 +40,6 @@ public class ExtraDetail {
 
     public void setmFullDescription(String mFullDescription) {
         this.mFullDescription = mFullDescription;
-    }
-
-    public String getmGallery1() {
-        return mGallery1;
-    }
-
-    public void setmGallery1(String mGallery1) {
-        this.mGallery1 = mGallery1;
-    }
-
-    public String getmGallery2() {
-        return mGallery2;
-    }
-
-    public void setmGallery2(String mGallery2) {
-        this.mGallery2 = mGallery2;
-    }
-
-    public String getmGallery3() {
-        return mGallery3;
-    }
-
-    public void setmGallery3(String mGallery3) {
-        this.mGallery3 = mGallery3;
-    }
-
-    public String getmGallery4() {
-        return mGallery4;
-    }
-
-    public void setmGallery4(String mGallery4) {
-        this.mGallery4 = mGallery4;
     }
 
     public String getmShortDescription() {
