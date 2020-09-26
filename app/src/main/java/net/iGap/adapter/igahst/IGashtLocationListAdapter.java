0d9b79d9d431824e55ec9605881219740c243895
nazanin <n.omrani>
2020-09-26 16:30:27 +0330

    change image in place holder

--- app/src/main/java/net/iGap/adapter/igahst/IGashtLocationListAdapter.java
+++ app/src/main/java/net/iGap/adapter/igahst/IGashtLocationListAdapter.java
@@ -61,9 +61,9 @@ public class IGashtLocationListAdapter extends RecyclerView.Adapter<RecyclerView
             ((ViewHolderLocationItem) viewHolder).itemAddressTextView.setText(items.get(i - 1).getAddressWithLanguage());
             ((ViewHolderLocationItem) viewHolder).itemLocationTextView.setText(items.get(i - 1).getLocation());
             if (items.get(i - 1).getmExtraDetail() != null) {
-                Picasso.with(G.context).load(items.get(i - 1).getmExtraDetail().getmCoverImage()).placeholder(R.drawable.logo_igap).error(R.drawable.ic_error_igap).fit().centerInside().into(((ViewHolderLocationItem) viewHolder).itemImageView);
+                Picasso.with(G.context).load(items.get(i - 1).getmExtraDetail().getmCoverImage()).placeholder(R.drawable.logo_igap_small).error(R.drawable.ic_error_igap).fit().centerInside().into(((ViewHolderLocationItem) viewHolder).itemImageView);
             } else {
-                Picasso.with(G.context).load(R.drawable.logo_igap).into(((ViewHolderLocationItem) viewHolder).itemImageView);
+                Picasso.with(G.context).load(R.drawable.logo_igap_small).into(((ViewHolderLocationItem) viewHolder).itemImageView);
             }
             ((ViewHolderLocationItem) viewHolder).buyTicketButton.setOnClickListener(v -> locationItemClickListener.buyTicket(viewHolder.getAdapterPosition() - 1));
             viewHolder.itemView.setOnClickListener(v -> locationItemClickListener.onItem(viewHolder.getAdapterPosition() - 1));
