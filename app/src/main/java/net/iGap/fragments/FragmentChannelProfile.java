cc4b391ceab782bdb3a4029bb74711c745d53da2
Mr.Abbaszadeh <mod.abbaszadeh@gmail.com>
2020-10-14 14:53:33 +0330

    change the logic of copying link

--- app/src/main/java/net/iGap/fragments/FragmentChannelProfile.java
+++ app/src/main/java/net/iGap/fragments/FragmentChannelProfile.java
@@ -227,11 +227,12 @@ public class FragmentChannelProfile extends BaseFragment implements OnChannelAva
                 layoutChannelLink.addView(inputChannelLink, layoutParams);
                 layoutChannelLink.addView(txtLink, layoutParams);
 
-
-                ClipboardManager clipboard = (ClipboardManager) getActivity().getSystemService(CLIPBOARD_SERVICE);
-                ClipData clip = ClipData.newPlainText("LINK_GROUP", link);
-                clipboard.setPrimaryClip(clip);
-                Toast.makeText(getContext(), R.string.copied, Toast.LENGTH_SHORT).show();
+                if (getActivity() != null) {
+                    ClipboardManager clipboard = (ClipboardManager) getActivity().getSystemService(CLIPBOARD_SERVICE);
+                    ClipData clip = ClipData.newPlainText("LINK_GROUP", link);
+                    clipboard.setPrimaryClip(clip);
+                    Toast.makeText(getActivity(), R.string.copied, Toast.LENGTH_SHORT).show();
+                }
             }
         });
 
