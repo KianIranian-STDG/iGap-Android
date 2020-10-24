abdc9dd8299df829efd490428ceb23779b10c5fe
Mohammad mirdar <mohammad_mirdar70@yahoo.com>
2020-10-24 17:29:12 +0330

    some minor ui changes in refund submit dialog

--- app/src/main/java/net/iGap/kuknos/viewmodel/KuknosRefundHistoryVM.java
+++ app/src/main/java/net/iGap/kuknos/viewmodel/KuknosRefundHistoryVM.java
@@ -12,31 +12,48 @@ public class KuknosRefundHistoryVM extends BaseAPIViewModel {
 
     private PanelRepo panelRepo = new PanelRepo();
     private MutableLiveData<KuknosRefundHistory> refundHistory;
+    private MutableLiveData<String> errorState;
+    private MutableLiveData<Boolean> progressState;
 
     public KuknosRefundHistoryVM() {
         this.refundHistory = new MutableLiveData<>();
+        this.progressState = new MutableLiveData<>();
+        this.errorState = new MutableLiveData<>();
+        progressState.setValue(true);
     }
 
     public void getUserRefundsFromServer() {
+        progressState.setValue(true);
         panelRepo.getUserRefunds(panelRepo.getUserRepo().getAccountID(), this, new ResponseCallback<KuknosResponseModel<KuknosRefundHistory>>() {
             @Override
             public void onSuccess(KuknosResponseModel<KuknosRefundHistory> data) {
                 refundHistory.setValue(data.getData());
+                progressState.setValue(false);
             }
 
             @Override
             public void onError(String error) {
-
+                progressState.setValue(false);
+                errorState.setValue(error);
             }
 
             @Override
             public void onFailed() {
-
+                progressState.setValue(false);
+                errorState.setValue("Process Failed");
             }
         });
     }
 
     public MutableLiveData<KuknosRefundHistory> getRefundHistory() {
         return refundHistory;
+    }
+
+    public MutableLiveData<String> getErrorState() {
+        return errorState;
+    }
+
+    public MutableLiveData<Boolean> getProgressState() {
+        return progressState;
     }
 }
