db10221cfe49aa26023ace3ed3aadd78998bdbcc
nazanin <n.omrani>
2021-01-05 16:47:33 +0330

    deleted class

--- app/src/main/java/net/iGap/structs/ContactObject.java
+++
@@ -1,9 +0,0 @@
-package net.iGap.structs;
-
-import net.iGap.proto.ProtoGlobal;
-
-public class ContactObject {
-    public static ContactObject create(ProtoGlobal.RoomMessageContact contact) {
-        return null;
-    }
-}
