1a971322c4e0d2fba2cedd9fb2e4d7ad643b25ee
nazanin <n.omrani>
2021-01-05 16:40:26 +0330

    created RoomContactObject class for getContact in MessageObject

---
+++ app/src/main/java/net/iGap/structs/RoomContactObject.java
@@ -0,0 +1,40 @@
+package net.iGap.structs;
+
+import net.iGap.proto.ProtoGlobal;
+import net.iGap.realm.RealmRoomMessageContact;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RoomContactObject {
+    public String firstName;
+    public String lastName;
+    public String nickName;
+    public List<String> phones = new ArrayList<>();
+    public List<String> emails = new ArrayList<>();
+
+    public static RoomContactObject create(ProtoGlobal.RoomMessageContact contact) {
+        return null;
+    }
+
+    public static RoomContactObject create(RealmRoomMessageContact roomMessageContact) {
+        if (roomMessageContact == null) {
+            return null;
+        }
+
+        RoomContactObject roomContactObject = new RoomContactObject();
+        roomContactObject.firstName = roomMessageContact.getFirstName();
+        roomContactObject.lastName = roomMessageContact.getLastName();
+        roomContactObject.nickName = roomMessageContact.getNickName();
+
+        for (int i = 0; i < roomMessageContact.getPhones().size(); i++) {
+            roomContactObject.phones.add(roomMessageContact.getPhones().get(i).getString());
+        }
+
+        for (int i = 0; i < roomMessageContact.getEmails().size(); i++) {
+            roomContactObject.emails.add(roomMessageContact.getEmails().get(i).getString());
+        }
+
+        return roomContactObject;
+    }
+}
