9fbc0782092e2774890f7a6d0fc4a232851494e3
Shokoofe Adeli <shokoofe.adeli@gmail.com>
2021-05-22 14:40:33 +0430

    correct format in humanReadableByteCount function

--- app/src/main/java/net/iGap/module/AndroidUtils.java
+++ app/src/main/java/net/iGap/module/AndroidUtils.java
@@ -428,7 +428,7 @@ public final class AndroidUtils {
         int unit = (!si ? 1000 : 1024);
         if (bytes < unit) return bytes + " B";
         int exp = (int) (Math.log(bytes) / Math.log(unit));
-        String pre = (!si ? "kMGTPE" : "KMGTPE").charAt(exp - 1) + (!si ? "" : "i");
+        String pre = ("KMGTPE").charAt(exp - 1) + (si ? "" : "i");
         return String.format("%.1f %sB", bytes / Math.pow(unit, exp), pre);
     }
 
