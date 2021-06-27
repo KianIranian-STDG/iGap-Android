03d89dc7dfdfced87ad9bb17834e16815b71167b
Shokoofe Adeli <shokoofe.adeli@gmail.com>
2021-06-27 14:46:28 +0430

    fix bug: Remove "arrow" button.          "Add" caption should be changed to "Account".          Clicking on "Add Account" should open add account bottom dialog.

--- app/src/main/java/net/iGap/viewmodel/UserProfileViewModel.java
+++ app/src/main/java/net/iGap/viewmodel/UserProfileViewModel.java
@@ -519,10 +519,6 @@ public class UserProfileViewModel extends ViewModel implements RefreshWalletBala
         goToSettingPage.setValue(true);
     }
 
-    public void onAddClick() {
-        goToAddMemberPage.setValue(true);
-    }
-
     public void onCreditClick() {
         if (!userInfo.isWalletRegister()) {
             goToWalletAgreementPage.setValue(HelperNumerical.getPhoneNumberStartedWithZero(phoneNumber));
