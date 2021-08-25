49dd6a607cc42a7918bc4bdd92593bf476ff30b8
Mohammad Mirdar <mohammadmirdarmpanahi@gmail.com>
2021-08-25 13:43:05 +0430

    BUG FIXED -> wrong time view in single story from fragment chat bug fixed

--- app/src/main/java/net/iGap/realm/RealmStoryProto.java
+++ app/src/main/java/net/iGap/realm/RealmStoryProto.java
@@ -29,7 +29,7 @@ public class RealmStoryProto extends RealmObject {
         }
 
         realmStory.setCaption(story.getCaption());
-        realmStory.setCreatedAt(story.getCreatedAt());
+        realmStory.setCreatedAt(story.getCreatedAt() * 1000);
         realmStory.setFile(RealmAttachment.build(realm,story.getFileDetails(), AttachmentFor.MESSAGE_ATTACHMENT, ProtoGlobal.RoomMessageType.STORY));
         realmStory.setSeen(story.getSeen());
         realmStory.setFileToken(story.getFileToken());
