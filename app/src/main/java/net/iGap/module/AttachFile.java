7c8da04db8d6142c37233cd979753b36fcd85894
a.hosseini <a.hosseini@kianiranian.com>
2021-09-11 13:35:13 -0700

    fix bug in get user location and send user location and get best location from gps or network  IA-663

--- app/src/main/java/net/iGap/module/AttachFile.java
+++ app/src/main/java/net/iGap/module/AttachFile.java
@@ -40,6 +40,8 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
 
 import com.afollestad.materialdialogs.MaterialDialog;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 
 import net.iGap.G;
 import net.iGap.R;
@@ -91,6 +93,8 @@ public class AttachFile {
     private LocationManager locationManager;
     private ProgressDialog pd;
     private Boolean sendPosition = false;
+    private FusedLocationProviderClient fusedLocationClient;
+    Location locationGPS = null;
     LocationListener locationListener = new LocationListener() {
 
         @Override
@@ -252,7 +256,7 @@ public class AttachFile {
             } else {
                 InputStream input = G.context.getContentResolver().openInputStream(uri);
 
-                AndroidUtils.copyFile(input, new File(destinationPath),0,null);
+                AndroidUtils.copyFile(input, new File(destinationPath), 0, null);
             }
 
             return destinationPath;
@@ -709,14 +713,7 @@ public class AttachFile {
                 }
                 //locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
                 //locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
-                Location location =  getLastKnownLocation();
-                if (location == null) {
-                    locationManager.requestLocationUpdates(
-                            LocationManager.NETWORK_PROVIDER,
-                            0L,
-                            0L, (LocationListener) this);
-                    location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
-                }
+                Location location = getLastBestLocation();
                 if (location != null) {
                     location.getLatitude();
                     location.getLongitude();
@@ -738,20 +735,34 @@ public class AttachFile {
             e.printStackTrace();
         }
     }
-    private Location getLastKnownLocation() {
-        List<String> providers = locationManager.getProviders(true);
-        Location bestLocation = null;
-        for (String provider : providers) {
-            Location l = locationManager.getLastKnownLocation(provider);
-            if (l == null) {
-                continue;
-            }
-            if (bestLocation == null || l.getAccuracy() < bestLocation.getAccuracy()) {
-                // Found best last known location: %s", l);
-                bestLocation = l;
-            }
+
+    private Location getLastBestLocation() {
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(context);
+        fusedLocationClient.getLastLocation()
+                .addOnSuccessListener((com.google.android.gms.tasks.OnSuccessListener<? super Location>) location -> {
+                    // Got last known location. In some rare situations this can be null.
+                    if (location != null) {
+                        locationGPS = location;
+                    }
+                });
+        Location locationNet = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+
+        long GPSLocationTime = 0;
+        if (null != locationGPS) {
+            GPSLocationTime = locationGPS.getTime();
         }
-        return bestLocation;
+
+        long NetLocationTime = 0;
+
+        if (null != locationNet) {
+            NetLocationTime = locationNet.getTime();
+        }
+
+        if (0 < GPSLocationTime - NetLocationTime) {
+            return locationGPS;
+        } else {
+            return locationNet;
+        }
     }
 
     void showSettingsAlert(final Fragment fragment) {
